99b125043d63a78faf5adccc6f38be02f4402d12
Becky Marques <becky.marques@colmeiaperformance.com.br>
2022-03-18 15:17:13 -0300

    Ajuste para facilitar testes

--- page.php
+++ page.php
@@ -1,6 +1,24 @@
 <?php get_header(); ?>
 <?php get_template_part('template-parts/header-home'); ?>
 <main>
-    <?php get_template_part( 'template-parts/flexible-content' ) ?>
+<section class="px-5">
+  <div class="d-flex justify-content-center">
+    <div>
+
+      <?php 
+  if ( have_posts() ) {
+    while ( have_posts() ) {
+      the_post(); 
+      //
+      // Post Content here
+      the_content();
+      //
+    } // end while
+  } // end if
+  ?>
+
+</div>
+</div>
+</section>
 </main>
 <?php get_footer() ?>
