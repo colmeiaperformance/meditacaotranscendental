2eaf665d45f5cb6963916105d9a8d16b61a4dce3
Becky Marques <becky.marques@colmeiaperformance.com.br>
2022-04-19 12:31:58 -0300

    Update page.php

--- page.php
+++ page.php
@@ -2,6 +2,21 @@
 <?php get_template_part('template-parts/navbar'); ?>
 <?php get_template_part('template-parts/headers/header-home'); ?>
 <main class="page">
-    <?php get_template_part( 'template-parts/flexible-content' ) ?>
+  <div>
+
+    <?php 
+  if ( have_posts() ) {
+    while ( have_posts() ) {
+      the_post(); 
+      //
+      // Post Content here
+      the_content();
+      //
+    } // end while
+  } // end if
+  ?>
+
+  </div>
+  <?php get_template_part( 'template-parts/flexible-content' ) ?>
 </main>
 <?php get_footer() ?>
