5effa3c4cb70c32224a5c4821012eb7a86d0a9b6
Igor Fonseca <igor.stutzf@gmail.com>
2022-04-28 18:17:36 -0300

    update sectioncomo-aprender, create scss mt-como-aprender

--- template-parts/sections/home/como-aprender.php
+++ template-parts/sections/home/como-aprender.php
@@ -1,28 +1,28 @@
-<section class="como-aprender">
+<section class="como-aprender py-5">
   <div class="container">
-    <h2>Como aprender <strong>MT</strong> ?</h2>
-    <div class="row align-items-center justify-content-center">
+    <div class="col-12 d-flex align-items-center justify-content-center title">
+      <h2>Como aprender <strong><span>MT</span></strong>?</h2>
+    </div>
+    <div class="row mt-5 d-flex align-items-center justify-content-center align-items-stretch">
 
-      <div class="col-12 col-lg-6 d-flex">
-        <div class="col-4">
+      <div class="col-12 col-lg-6 d-flex flex-column flex-lg-row align-items-center justify-content-center text-center text-lg-start">
+        <div class="col-12 col-lg-4 como-aprender-imagem mb-5">
           <img class="img-fluid" src=<?php echo get_template_directory_uri() . '/images/aprenda-mt/icon-arvore.png' ?>
             alt="Aprenda MT">
         </div>
-        <div class="col-8 como-aprender-texto">
+        <div class="col-12 col-lg-8 como-aprender-texto mb-4">
           <p>A Meditação Transcendental é ensinada somente por professores credenciados pela Associação Internacional de
             Meditação (SIM).</p>
           <p>Mais que um curso.</br>
             O início de uma jornada de autoconhecimento e evolução.</p>
-
         </div>
       </div>
 
-      <div class="col-12 col-lg-6">
+      <div class="col-12 col-lg-6 lista">
         <div>
           <ul class="">
             <li class="">O curso é realizado em 4 encontros presenciais.</li>
-            <li class="">O primeiro dia é individual e os demais podem ser em pequenos grupos.
-            </li>
+            <li class="">O primeiro dia é individual e os demais podem ser em pequenos grupos.</li>
             <li class="">Você aprende a meditar através da experiência prática com a MT.</li>
             <li class="">Após o curso, você terá acompanhamento sempre que precisar.</li>
           </ul>
@@ -31,14 +31,16 @@
       </div>
 
     </div>
-    <div class="row text-center">
+
+    <div class="row text-call mt-sm-4 text-center">
       <p>
-        O <strong>primeiro passo</strong> para aprender é participar de uma <strong>palestra online gratuita</strong>.
+        O <b>primeiro passo</b> para aprender é participar de uma <strong>palestra online gratuita</strong>.
       </p>
     </div>
+
     <div class="row justify-content-center">
       <div class="col-12 text-center">
-        <a class="btn" href="#" role="button">Inscreva-se</a>
+        <a class="btn px-4" href="#" role="button"><b>Inscreva-se</b></a>
       </div>
     </div>
   </div>
