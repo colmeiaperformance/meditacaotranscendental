a184d53e204550d308a0f058209232386077501b
Becky Marques <becky.marques@colmeiaperformance.com.br>
2022-04-28 12:58:51 -0300

    Add section html structure como-aprender.php

--- template-parts/flexible-content.php
+++ template-parts/flexible-content.php
@@ -1,4 +1,5 @@
 <?php get_template_part('template-parts/sections/home/mt-unica/mt-unica'); ?>
 <?php get_template_part('template-parts/sections/home/mt-unica/mt-unica-slide'); ?>
 <?php get_template_part('template-parts/sections/home/mt-unica/mt-unica-depoimentos'); ?>
+<?php get_template_part('template-parts/sections/home/como-aprender'); ?>
 <?php get_template_part('template-parts/sections/home/transcender'); ?>
---
+++ template-parts/sections/home/como-aprender.php
@@ -0,0 +1,45 @@
+<sectin class="como-aprender">
+  <div class="container">
+    <h2>Como aprender <strong>MT</strong> ?</h2>
+    <div class="row align-items-center justify-content-center">
+
+      <div class="col-12 col-lg-6 d-flex">
+        <div class="col-4">
+          <img class="img-fluid" src=<?php echo get_template_directory_uri() . '/images/aprenda-mt/icon-arvore.png' ?>
+            alt="Aprenda MT">
+        </div>
+        <div class="col-8 como-aprender-texto">
+          <p>A Meditação Transcendental é ensinada somente por professores credenciados pela Associação Internacional de
+            Meditação (SIM).</p>
+          <p>Mais que um curso.</br>
+            O início de uma jornada de autoconhecimento e evolução.</p>
+
+        </div>
+      </div>
+
+      <div class="col-12 col-lg-6">
+        <div>
+          <ul class="">
+            <li class="">O curso é realizado em 4 encontros presenciais.</li>
+            <li class="">O primeiro dia é individual e os demais podem ser em pequenos grupos.
+            </li>
+            <li class="">Você aprende a meditar através da experiência prática com a MT.</li>
+            <li class="">Após o curso, você terá acompanhamento sempre que precisar.</li>
+          </ul>
+        </div>
+
+      </div>
+
+    </div>
+    <div class="row text-center">
+      <p>
+        O <strong>primeiro passo</strong> para aprender é participar de uma <strong>palestra online gratuita</strong>.
+      </p>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-12 text-center">
+        <a class="btn" href="#" role="button">Inscreva-se</a>
+      </div>
+    </div>
+  </div>
+</section>
