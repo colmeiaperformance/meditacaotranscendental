434d20702ded7c2385cf564d362e3d91f1ba27c1
Igor Fonseca <igor.stutzf@gmail.com>
2022-04-28 20:47:30 -0300

    create mt-o-que-dizem.scss, uptade o-que-dizem.php

--- template-parts/sections/home/o-que-dizem.php
+++ template-parts/sections/home/o-que-dizem.php
@@ -1,19 +1,67 @@
-<section class="o-que-dizem">
+<section class="o-que-dizem py-5">
   <div class="container">
-    <h2>O que dizem sobre a <strong>MT</strong> ?</h2>
+   <div class="col-12 d-flex align-items-center justify-content-center title">
+      <h2>O que dizem sobre a <strong>MT</strong> ?</h2>
+   </div>
     <div class="row">
 
       <div class="col-12">
-        <div class="">
-          Espaço para carousel
-        </div>
+
+        <!-- inicio carousel -->
+        <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+          
+          <div class="carousel-inner">
+            <div class="carousel-item ">
+            <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
+
+              <div class="container">
+                <div class="carousel-caption">
+                  <div class="teste"><h1>CONTEUDO 1.</h1> </div>
+                  <div class="teste"><h1>CONTEUDO 2.</h1> </div>
+                  <div class="teste"><h1>CONTEUDO 3.</h1> </div>              
+                </div>
+              </div>
+            </div>
+            <div class="carousel-item active">
+              <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
+
+              <div class="container">
+                <div class="carousel-caption">
+                  <div><h1>CONTEUDO 1.</h1> </div>
+                  <div><h1>CONTEUDO 2.</h1> </div>
+                  <div><h1>CONTEUDO 3.</h1> </div>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
+
+              <div class="container">
+                <div class="carousel-caption">
+                  <div><h1>CONTEUDO 1.</h1> </div>
+                  <div><h1>CONTEUDO 2.</h1> </div>
+                  <div><h1>CONTEUDO 3.</h1> </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+  </div>
+  <!-- final carousel -->
       </div>
 
     </div>
 
     <div class="row">
       <div class="col-12 text-center">
-        <a class="btn" href="#" role="button">Encontre um instrutor credenciado</a>
+        <a class="btn px-4" href="#" role="button"><b>Encontre um instrutor credenciado</b></a>
       </div>
     </div>
 
