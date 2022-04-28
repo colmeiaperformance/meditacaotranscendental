e3b9031aa4ed6921f81b734294998b7c155294e6
Becky Marques <becky.marques@colmeiaperformance.com.br>
2022-04-28 13:18:05 -0300

    Add section html structure o-que-dizem.php

--- template-parts/flexible-content.php
+++ template-parts/flexible-content.php
@@ -2,4 +2,5 @@
 <?php get_template_part('template-parts/sections/home/mt-unica/mt-unica-slide'); ?>
 <?php get_template_part('template-parts/sections/home/mt-unica/mt-unica-depoimentos'); ?>
 <?php get_template_part('template-parts/sections/home/como-aprender'); ?>
+<?php get_template_part('template-parts/sections/home/o-que-dizem'); ?>
 <?php get_template_part('template-parts/sections/home/transcender'); ?>
---
+++ template-parts/sections/home/o-que-dizem.php
@@ -0,0 +1,21 @@
+<section class="o-que-dizem">
+  <div class="container">
+    <h2>O que dizem sobre a <strong>MT</strong> ?</h2>
+    <div class="row">
+
+      <div class="col-12">
+        <div class="">
+          Espaço para carousel
+        </div>
+      </div>
+
+    </div>
+
+    <div class="row">
+      <div class="col-12 text-center">
+        <a class="btn" href="#" role="button">Encontre um instrutor credenciado</a>
+      </div>
+    </div>
+
+  </div>
+</section>
