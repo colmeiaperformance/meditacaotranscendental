c39283b707d4577b54e68a70c69254c156cc2cac
brunarafaela <brunarafaelav@outlook.com>
2022-04-30 02:42:38 -0300

    Add conheca-beneficios-section

---
+++ template-parts/sections/home/conheca-beneficios.php
@@ -0,0 +1,41 @@
+<section class="conheca-beneficios">
+  <div class="container">
+    <h2>Conheça os <strong>benefícios</strong> da MT</h2>
+    <div class="row align-items-center justify-content-center">
+      <div class="col-6 col-md-4 align-items-center justify-content-center">
+        <div class="rounded-circle">
+          <img class="img-fluid" src=<?php echo get_template_directory_uri() . '/images/beneficios/mental.png' ?>
+            alt="Benefícios: Saúde Mental">
+          <p>Saúde Mental</p>
+        </div>
+      </div>
+      <div class="col-6 col-md-4 align-items-center justify-content-center">
+         <div class="rounded-circle">
+          <img class="img-fluid" src=<?php echo get_template_directory_uri() . '/images/beneficios/mental.png' ?>
+            alt="Benefícios: Saúde Mental">
+          <p>Saúde Mental</p>
+        </div>
+      </div>
+      <div class="col-6 col-md-4 align-items-center justify-content-center">
+         <div class="rounded-circle">
+          <img class="img-fluid" src=<?php echo get_template_directory_uri() . '/images/beneficios/mental.png' ?>
+            alt="Benefícios: Saúde Mental">
+          <p>Saúde Mental</p>
+        </div>
+      </div>
+      <div class="col-6 col-md-4 align-items-center justify-content-center">
+         <div class="rounded-circle">
+          <img class="img-fluid" src=<?php echo get_template_directory_uri() . '/images/beneficios/mental.png' ?>
+            alt="Benefícios: Saúde Mental">
+          <p>Saúde Mental</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="quote">
+      <p>O propósito da vida é a expansão da felicidade</p>
+      <h4>Maharishi Mahesh Yogi</h4>
+    </div>
+    </div>
+  </div>
+</section>
