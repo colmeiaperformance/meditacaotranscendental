c66682143e60414c111c1df0cafaf7b9311b9ed1
brunarafaela <brunarafaelav@outlook.com>
2022-05-02 04:59:07 -0300

    Add slides seção única slide

--- template-parts/sections/home/mt-unica/mt-unica-slide.php
+++ template-parts/sections/home/mt-unica/mt-unica-slide.php
@@ -2,12 +2,12 @@
   <div id="mt-unica-slide" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src=<?php echo get_template_directory_uri() . '/images/mt-unica/mt-unica-bg.png' ?> alt="Vídeo" width="100%" height="100%" class="bd-placeholder-img">
+        <img src=<?php echo get_template_directory_uri() . '/images/mt-unica/slide1.png' ?> alt="Vídeo" width="100%" height="100%" class="bd-placeholder-img">
         <div class="container">
           <div class="row">
             <div class="col-6 carousel-caption text-start">
-              <h3>Mais de 10 milhões de pessoas praticam a MT em todo mundo</h3>
-              <p>A MT não é religião e não envolve crenças nem mudanças no estilo de vida. É para todos!</p>
+              <h3>Totalmente sem esforço e agradável de praticar</h3>
+              <p>Mesmo os mais ansiosos e com a mente muito ativa, podem meditar. A MT é uma técnica natural, que não envolve concentração ou controle da mente.</p>
               <a class="btn" href="#">Participe da palestra gratuita</a>
             </div>
           </div>
@@ -15,7 +15,59 @@
       </div>
      
       <div class="carousel-item">
-        <img src=<?php echo get_template_directory_uri() . '/images/mt-unica/mt-unica-bg.png' ?> alt="Vídeo" width="100%" height="100%" class="bd-placeholder-img">
+        <img src=<?php echo get_template_directory_uri() . '/images/mt-unica/slide2.png' ?> alt="Vídeo" width="100%" height="100%" class="bd-placeholder-img">
+        <div class="container">
+          <div class="row">
+            <div class="col-6 carousel-caption text-start">
+              <h3>Você pode meditar em qualquer lugar</h3>
+              <p>Em casa, no trabalho, na escola, no transporte. Basta sentar confortavelmente com os olhos fechados.</p>
+              <a class="btn" href="#">Participe da palestra gratuita</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="carousel-item">
+        <img src=<?php echo get_template_directory_uri() . '/images/mt-unica/slide3.png' ?> alt="Vídeo" width="100%" height="100%" class="bd-placeholder-img">
+        <div class="container">
+          <div class="row">
+            <div class="col-6 carousel-caption text-start">
+              <h3>Instrução individual e personalizada</h3>
+              <p>Você recebe a técnica de forma individualizada por instrutores certificados.</p>
+              <a class="btn" href="#">Participe da palestra gratuita</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="carousel-item">
+        <img src=<?php echo get_template_directory_uri() . '/images/mt-unica/slide4.png' ?> alt="Vídeo" width="100%" height="100%" class="bd-placeholder-img">
+        <div class="container">
+          <div class="row">
+            <div class="col-6 carousel-caption text-start">
+              <h3>Resultados comprovados por evidências científicas</h3>
+              <p>Mais de 600 estudos científicos atestam os benefícios da MT para o corpo e a mente.</p>
+              <a class="btn" href="#">Participe da palestra gratuita</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+       <div class="carousel-item">
+        <img src=<?php echo get_template_directory_uri() . '/images/mt-unica/slide5.png' ?> alt="Vídeo" width="100%" height="100%" class="bd-placeholder-img">
+        <div class="container">
+          <div class="row">
+            <div class="col-6 carousel-caption text-start">
+              <h3>Técnica milenar, com origem na tradição Védica</h3>
+              <p>Maharishi Mahesh Yogi, físico indiano e grande cientista da Consciência, reavivou esse conhecimento e o tornou acessível a todas as pessoas</p>
+              <a class="btn" href="#">Participe da palestra gratuita</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="carousel-item">
+        <img src=<?php echo get_template_directory_uri() . '/images/mt-unica/slide6.png' ?> alt="Vídeo" width="100%" height="100%" class="bd-placeholder-img">
         <div class="container">
           <div class="row">
             <div class="col-6 carousel-caption text-start">
