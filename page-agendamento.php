5a93d65b31d5a4189e377f682750e3dce3a0c145
brunarafaela <brunarafaelav@outlook.com>
2022-05-03 20:06:55 -0300

    Update page-instrutores

--- page-agendamento.php
+++ page-agendamento.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 <?php get_template_part('template-parts/navbar'); ?>
-<main class="page-instrutores">
+<main class="page-agendamento">
   <section class="instrutores-header">
     <div class="container">
-      <h2>Encontre um<br /> <strong>instrutor credenciado</strong></h2>
+      <h2>Encontre um <strong>instrutor credenciado</strong></h2>
       <p>Todos os instrutores são certificados pela <strong>Maharishi European Research University (MERU)</strong>, na Holanda, e credenciados pela <Strong>Associação Internacional de Meditação (SIM)</Strong>. </p>
     </div>
   </section>
@@ -247,35 +247,5 @@
       </div>
     </div>
   </section>
-    <script>
-  jQuery(document).ready(function() {
-    var swiper = new Swiper(".swiperInstrutores", {
-      slidesPerView: 1,
-      slidesPerGroup: 1,
-      loop: true,
-      spaceBetween: 5,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      breakpoints: {
-        576: {
-          slidesPerView: 2,
-          slidesPerGroup: 2,
-          spaceBetween: 5,
-        },
-        992: {
-          slidesPerView: 4,
-          slidesPerGroup: 4,
-          spaceBetween: 5,
-        },
-      },
-    });
-  });
-  </script>
 </main>
 <?php get_footer();
