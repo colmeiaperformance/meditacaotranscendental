25c77731d2bda4bfeb0eba20f4761936d49e2242
brunarafaela <brunarafaelav@outlook.com>
2022-05-03 21:03:54 -0300

    Update page-instrutor

---
+++ page-instrutor.php
@@ -0,0 +1,130 @@
+<?php get_header(); ?>
+<?php get_template_part('template-parts/navbar'); ?>
+<main class="page-instrutor">
+  <section class="instrutor-profile">
+    <div class="container">
+
+    </div>
+  </section>
+  <section class="instrutor-contact">
+    <div class="container">
+
+    </div>
+  </section>
+  <section class="instrutor-depoimentos">
+    <div id="instrutor-depoimentos" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+
+        <div class="carousel-item active text-center">
+          <div class="container">
+            <div class="depoimento d-block depoimento text-center">
+              <div class="quote"></div>
+              <p>Talvez o maior benefício seja o fato de que a MT é relativamente rápida de se aprender e fácil de se dominar. Não é necessário meses de prática para perceber os resultados.</p>
+              <h6><strong>Carlos Almeida</strong>, Aluno</h6>
+            </div>
+          </div>
+        </div>
+
+        <div class="carousel-item text-center">
+          <div class="container">
+            <div class="depoimento d-block depoimento text-center">
+              <div class="quote"></div>
+              <p>Talvez o maior benefício seja o fato de que a MT é relativamente rápida de se aprender e fácil de se dominar. Não é necessário meses de prática para perceber os resultados.</p>
+              <h6><strong>Carlos Almeida</strong>, Aluno</h6>
+            </div>
+          </div>
+        </div>
+
+        <div class="carousel-item text-center">
+          <div class="container">
+            <div class="depoimento d-block depoimento text-center">
+              <div class="quote"></div>
+              <p>Talvez o maior benefício seja o fato de que a MT é relativamente rápida de se aprender e fácil de se dominar. Não é necessário meses de prática para perceber os resultados.</p>
+              <h6><strong>Carlos Almeida</strong>, Aluno</h6>
+            </div>
+          </div>
+        </div>
+
+      <button class="carousel-control-prev" type="button" data-bs-target="#instrutor-depoimentos" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#instrutor-depoimentos" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+        <div class="carousel-indicators">
+        <button type="button" data-bs-target="#instrutor-depoimentos" data-bs-slide-to="0" class="" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#instrutor-depoimentos" data-bs-slide-to="1" aria-label="Slide 2" class="active" aria-current="true"></button>
+        <button type="button" data-bs-target="#instrutordepoimentos" data-bs-slide-to="2" aria-label="Slide 3" class="active" aria-current="true"></button>
+      </div>
+    </div>
+  </section>
+  <section class="instrutor-depoimentos">
+    <div id="instrutor-depoimentos-video" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+
+        <div class="carousel-item active">
+          <div class="container">
+            <div class="row d-flex align-items-center justify-content-center mb-5 px-5">
+              <div class="col-12 col-lg-7 depoimento depoimento-video d-block text-center text-lg-end mb-4 mb-lg-0">
+                <p>Talvez o maior benefício seja o fato de que a MT é relativamente rápida de se aprender e fácil de se dominar. Não é necessário meses de prática para perceber os resultados.</p>
+                <h6><strong>Carlos Almeida</strong>, Aluno</h6>
+              </div>
+              <div class="col-12 col-lg-5">
+                <iframe width="100%" height="255px" src="https://www.youtube.com/embed/YDicaTzD9tw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="carousel-item">
+          <div class="container">
+            <div class="row d-flex align-items-center justify-content-center mb-5 px-5">
+              <div class="col-12 col-lg-7 depoimento depoimento-video d-block text-center text-lg-end mb-4 mb-lg-0">
+                <p>Talvez o maior benefício seja o fato de que a MT é relativamente rápida de se aprender e fácil de se dominar. Não é necessário meses de prática para perceber os resultados.</p>
+                <h6><strong>Carlos Almeida</strong>, Aluno</h6>
+              </div>
+              <div class="col-12 col-lg-5">
+                <iframe width="100%" height="255px" src="https://www.youtube.com/embed/YDicaTzD9tw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div>
+            </div>
+          </div>
+        </div>
+
+         <div class="carousel-item">
+          <div class="container">
+            <div class="row d-flex align-items-center justify-content-center mb-5 px-5">
+              <div class="col-12 col-lg-7 depoimento depoimento-video d-block text-center text-lg-end mb-4 mb-lg-0">
+                <p>Talvez o maior benefício seja o fato de que a MT é relativamente rápida de se aprender e fácil de se dominar. Não é necessário meses de prática para perceber os resultados.</p>
+                <h6><strong>Carlos Almeida</strong>, Aluno</h6>
+              </div>
+              <div class="col-12 col-lg-5">
+                <iframe width="100%" height="255px" src="https://www.youtube.com/embed/YDicaTzD9tw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      <button class="carousel-control-prev" type="button" data-bs-target="#instrutor-depoimentos-video" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#instrutor-depoimentos-video" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+        <div class="carousel-indicators">
+        <button type="button" data-bs-target="#instrutor-depoimentos-video" data-bs-slide-to="0" class="" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#instrutor-depoimentos-video" data-bs-slide-to="1" aria-label="Slide 2" class="active" aria-current="true"></button>
+        <button type="button" data-bs-target="#instrutordepoimentos-video" data-bs-slide-to="2" aria-label="Slide 3" class="active" aria-current="true"></button>
+      </div>
+    </div>
+  </section>
+  <section class="instrutor-footer">
+    <div class="container text-center">
+      <a href="#" class="btn" role="button">Agende a palestra gratuita</a>
+    </div>
+  </section>
+</main>
+<?php get_footer();
