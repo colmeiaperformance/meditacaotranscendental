ebbf8adfcf41c5506c4f850d51d9d2ee68afea6c
brunarafaela <brunarafaelav@outlook.com>
2022-05-05 04:15:50 -0300

    Add onde atua section

--- page-oqueemt.php
+++ page-oqueemt.php
@@ -36,6 +36,25 @@ Template Name: O que é MT
       <div class="mundo-melhor-bg position-absolute"></div>
     </div>
   </section>
+  <section class="oqueemt-atua">
+    <div class="container">
+      <h2>Como a MT atua no <strong>cérebro</strong>?</h2>
+      <div class="row d-flex align-items-end justify-content-center">
+        <div class="col-12 col-lg-5 d-none d-lg-block">
+           <img class="img-fluid" src=<?php echo get_template_directory_uri() . '/images/oqueemt/cerebro.png' ?> alt="Cérebro">
+        </div>
+         <div class="col-12 col-lg-7">
+           <p>O acúmulo de estresse e de ansiedade é um fator que afeta toda nossa vida e, principalmente, nossa capacidade de pensar, agir, sentir e de nos relacionar.</p>
+           <p>Nosso cérebro age como se estivéssemos em constante perigo, ativando mais a função da amígdala, área do cérebro responsável pelo medo e pelos instintos, prejudicando nossa saúde.</p>
+           <div class="d-block d-lg-none text-center">
+            <img class="img-fluid" src=<?php echo get_template_directory_uri() . '/images/oqueemt/cerebro.png' ?> alt="Cérebro">
+           </div>
+           <p><strong>A prática regular da MT aumenta a atividade do córtex pré-frontal</strong> (região cérebro responsável por decisões, foco, inteligência e criatividade); </p>
+           <p><strong>Reduz a atividade da amígdala</strong>, dando mais equilíbrio emocional ao dia a dia.</p>
+        </div>
+      </div>
+    </div>
+  </section>
   <section class="oqueemt-imagine">
     <div class="container text-center">
       <h2>Imagine a mente como um grande oceano. Transcender é ir além da sua superfície.</h2>
