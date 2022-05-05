7eed7ef4abeab700b680a0483e13375053d9d8d0
brunarafaela <brunarafaelav@outlook.com>
2022-05-05 03:28:16 -0300

    Add imagine section

--- page-oqueemt.php
+++ page-oqueemt.php
@@ -19,23 +19,30 @@ Template Name: O que é MT
       </div>
     </div>
   </section>
-  <section class="mt-diferente position-relative">
-  <div class="mundo-melhor-topo">
-    <img src="<?php echo get_template_directory_uri() . '/images/oqueemt/mt-diferente-mobile.png' ?>" alt="mt.">
-  </div>
-  <div class="container">
-    <div class="row align-items-center justify-content-start">
-      <div class="col-12 col-lg-7">
-       <div class="text-center text-lg-start">
-          <h2>Por que a MT é <strong>diferente</strong>?</h2>
-       </div>
-       <p>Existem diversas técnicas de meditação, mas elas são distintas e não produzem os mesmos resultados. A maioria envolve exercícios de relaxamento ou de respiração, narrativas guiadas ou algum tipo de esforço que faz com que a mente continue ativa na superfície, sem experimentar o verdadeiro silêncio interior.</p>
-       <p><strong>Nada disso é necessário na MT. Ela é uma forma natural, confortável e automática de satisfação mental e de relaxamento físico. Sem esforço, sem tentar controlar ou observar pensamentos.</strong></p>
-      </div>
+  <section class="oqueemt-diferente position-relative">
+    <div class="mundo-melhor-topo">
+      <img src="<?php echo get_template_directory_uri() . '/images/oqueemt/mt-diferente-mobile.png' ?>" alt="mt.">
     </div>
-    <div class="mundo-melhor-bg position-absolute"></div>
-  </div>
-</section>
+    <div class="container">
+      <div class="row align-items-center justify-content-start">
+        <div class="col-12 col-lg-7">
+        <div class="text-center text-lg-start">
+            <h2>Por que a MT é <strong>diferente</strong>?</h2>
+        </div>
+        <p>Existem diversas técnicas de meditação, mas elas são distintas e não produzem os mesmos resultados. A maioria envolve exercícios de relaxamento ou de respiração, narrativas guiadas ou algum tipo de esforço que faz com que a mente continue ativa na superfície, sem experimentar o verdadeiro silêncio interior.</p>
+        <p><strong>Nada disso é necessário na MT. Ela é uma forma natural, confortável e automática de satisfação mental e de relaxamento físico. Sem esforço, sem tentar controlar ou observar pensamentos.</strong></p>
+        </div>
+      </div>
+      <div class="mundo-melhor-bg position-absolute"></div>
+    </div>
+  </section>
+  <section class="oqueemt-imagine">
+    <div class="container text-center">
+      <h2>Imagine a mente como um grande oceano. Transcender é ir além da sua superfície.</h2>
+      <p>Praticar a MT é como submergir naturalmente dentro de si para encontrar, na mente, um lugar que está além do nível agitado dos pensamentos, onde experimentamos um estado de descanso muito profundo, que nos leva a acessar uma fonte abundante de energia, de criatividade e de bem-estar.</p>
+      <a href="#" class="btn" role="button">Aprenda a MT</a>
+    </div>
+  </section>
   <section class="oqueemt-footer">
     <div class="container text-center">
       <h2>Dúvidas?</h2>
