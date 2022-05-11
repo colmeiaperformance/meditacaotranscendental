d6e009d7d57e5eda740c40677b25c63be84e09db
Becky Marques <becky.marques@colmeiaperformance.com.br>
2022-05-11 03:23:33 -0300

    Counter agende-palestra

--- template-parts/sections/home/agende-palestra.php
+++ template-parts/sections/home/agende-palestra.php
@@ -4,7 +4,7 @@
       <div class="col-6 col-md-3 d-flex align-items-center justify-content-center">
         <div class="circle green rounded-circle">
           <div class="d-block text-center px-2">
-            <h4>+600</h4>
+            <h4>+<span class="counter">600</span></h4>
             <p>estudos científicos</p>
           </div>
         </div>
@@ -12,7 +12,7 @@
       <div class="col-6 col-md-3 d-flex align-items-center justify-content-center">
         <div class="circle red rounded-circle">
           <div class="d-block text-center px-2">
-            <h4>+10</h4>
+            <h4>+<span class="counter">10</span></h4>
             <p>milhões de meditantes</p>
           </div>
         </div>
@@ -20,7 +20,7 @@
       <div class="col-6 col-md-3 d-flex align-items-center justify-content-center">
         <div class="circle blue rounded-circle">
           <div class="d-block text-center px-2">
-            <h4>+100</h4>
+            <h4>+<span class="counter">100</span></h4>
             <p>países</p>
           </div>
         </div>
@@ -28,8 +28,8 @@
       <div class="col-6 col-md-3 d-flex align-items-center justify-content-center">
         <div class="circle yellow rounded-circle">
           <div class="d-block text-center px-2">
-            <h4>+100</h4>
-            <p>mil meditantes no brasil</p>
+            <h4>+<span class="counter">500</span></h4>
+            <p>mil meditantes no Brasil</p>
           </div>
         </div>
       </div>
@@ -43,3 +43,41 @@
 
   </div>
 </section>
+
+<script>
+jQuery(document).ready(function() {
+  // jQuery(".counter").counterUp({
+  //   delay: 10,
+  //   time: 1200
+  // });
+
+  const counterUp = window.counterUp.default
+
+  const callback = entries => {
+    entries.forEach(entry => {
+      const el = entry.target
+      if (entry.isIntersecting && !el.classList.contains('is-visible')) {
+        for (const counter of counters) {
+          counterUp(counter, {
+            duration: 1000,
+            delay: 10,
+          })
+          el.classList.add('is-visible')
+        }
+      }
+    })
+  }
+
+  // observer
+  const IO = new IntersectionObserver(callback, {
+    threshold: 1
+  })
+
+  // First element to target
+  const el = document.querySelector('.counter')
+
+  // all numbers
+  const counters = document.querySelectorAll('.counter')
+  IO.observe(el)
+});
+</script>
