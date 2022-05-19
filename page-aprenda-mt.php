fd6625b35f2773c2347c34103278fb9884379d3a
brunarafaela <brunarafaelav@outlook.com>
2022-05-19 02:15:45 -0300

    Add 3 passos section

--- page-aprenda-mt.php
+++ page-aprenda-mt.php
@@ -47,8 +47,80 @@
   </section>
   <section class="tres-passos">
     <div class="container">
-      ..
+      <h2><strong>3 passos</strong> para aprender a MT</h2>
     </div>
+      <div class="item">
+       <div class="container d-flex align-items-lg-center justify-content-start">
+          <span>1</span>
+        <div class="text">
+          <h4>Palestra gratuita</h4>
+          <p>Você vai entender como a MT funciona e os benefícios para a mente e o corpo.</p>
+        </div>
+       </div>
+      </div>
+      <div class="item">
+       <div class="container d-flex align-items-lg-center justify-content-start">
+          <span>
+          2
+        </span>
+        <div class="text">
+          <h4>Entrevista individual</h4>
+          <p>Um bate-papo com o instrutor credenciado para identificar suas necessidades.</p>
+        </div>
+       </div>
+      </div>
+      <div class="item">
+       <div class="container d-flex align-items-lg-center justify-content-start">
+          <span>
+          3
+        </span>
+        <div class="text">
+          <h4>Curso presencial</h4>
+          <p>São 4 encontros consecutivos de aproximadamente 1h30 cada.</p>
+        </div>
+        </div>
+      </div>
+
+    <div class="passos-aprender">
+      <div class="container">
+      <div class="swiper passosSwiper">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <div class="passo">
+              <h5>Dia 1</h5>
+              <p>Instrução da técnica de forma individualizada e personalizada.</p>
+            </div>
+          </div>
+
+          <div class="swiper-slide">
+            <div class="passo">
+              <h5>Dia 2</h5>
+              <p>Os aspectos da prática correta para benefícios máximos.</p>
+            </div>
+          </div>
+
+          <div class="swiper-slide">
+            <div class="passo">
+              <h5>Dia 3</h5>
+              <p>A mecânica da técnica na mente e no corpo para a dissolução do estresse.</p>
+            </div>
+          </div>
+
+          <div class="swiper-slide">
+            <div class="passo">
+              <h5>Dia 4</h5>
+              <p>Os efeitos no longo prazo para sua evolução individual e plenitude na vida.</p>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+      </div>
+    </div>
+    </div>
+     <div class="text-center">
+        <a href="#" class="btn">Conheça os benefícios</a>
+      </div>
   </section>
   <section class="mt-curso">
     <div class="container">
@@ -280,6 +352,41 @@
           slidesPerView: 4,
           slidesPerGroup: 1,
           spaceBetween: 40,
+        },
+      },
+    });
+  });
+</script>
+<script>
+  jQuery(document).ready(function() {
+    var swiper = new Swiper(".passosSwiper", {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      loop: false,
+      spaceBetween: 0,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          slidesPerGroup: 2,
+          spaceBetween: 0,
+        },
+        768: {
+          slidesPerView: 2,
+          slidesPerGroup: 2,
+          spaceBetween: 0,
+        },
+        992: {
+          slidesPerView: 4,
+          slidesPerGroup: 4,
+          spaceBetween: 45,
         },
       },
     });
