44d8d2100010886c7b1f3bb2faeb073be8df25c5
Bruna Rafaela <Bruna@MacBook-Pro-de-Bruna.local>
2022-05-31 09:42:46 -0300

    Add quem somos page

---
+++ page-quem-somos.php
@@ -0,0 +1,13 @@
+<?php
+  /*
+  Template Name: Quem Somos
+  */
+  ?>
+<?php get_header(); ?>
+<?php get_template_part('template-parts/navbar'); ?>
+<main class="page-quem-somos">
+  <section class="">
+    
+  </section>
+</main>
+<?php get_footer();
---
+++ template-parts/headers/header-quem-somos.php
@@ -0,0 +1,27 @@
+<header class="header header--quem-somos">
+  <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+    <!-- <div class="carousel-indicators">
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" class="active" aria-current="true"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div> -->
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img loading="lazy" class="img-fluid" src=<?php echo get_template_directory_uri() . '/images/header/banner-quemsomos.png' ?> alt="Instrutores">
+        <div class="container">
+          <div class="carousel-caption">
+            <h1>Uma técnica <strong>simples, fácil e natural.</strong></h1>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button> -->
+  </div>
+</header>
