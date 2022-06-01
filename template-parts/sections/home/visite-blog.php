da864e1a9115764e7262c29db5c1929a8e3773fe
Igor Fonseca <igor.stutzf@gmail.com>
2022-06-01 19:03:06 -0300

    update tipografia home, visite-blog.php

--- template-parts/sections/home/visite-blog.php
+++ template-parts/sections/home/visite-blog.php
@@ -1,12 +1,12 @@
 <section class="visite-blog">
     <div class="container py-5">
       <h2>Visite o nosso <strong>blog</strong></h2>
-      <div id="oquedizem" class="swiper swiperConhecaBlog" data-bs-ride="carousel">
+      <div id="conhecaBlog" class="swiper swiperConhecaBlog" data-bs-ride="carousel">
         <div class="swiper-wrapper">
 
           <div class="swiper-slide col-12 col-lg-6">
-            <div class="p-3 col d-flex">
-              <div class="post p-3 col-lg-12 d-lg-flex">
+            <div class="p-3 col d-flex h-100">
+              <div class="post p-3 d-lg-flex">
                 <div class="post-img col mb-2 mb-lg-0 col-lg-5">
                   <img loading="lazy" class="img-fluid" src="https://picsum.photos/800/500?random=1" alt="">
                 </div>
@@ -18,7 +18,7 @@
                       Conheça o cientista Dr. Tony Nader, chefe da Organização Internacional de Meditação Transcendental®
                     </p>
                   </div>
-                  <div class="item-btn d-flex justify-content-center justify-content-lg-end">
+                  <div class="item-btn d-flex align-items-end justify-content-center justify-content-lg-end">
                     <a class="btn visite-blog-btn text-uppercase" href="#" role="button">Leia +</a>
                   </div>
                 </div>
@@ -27,8 +27,8 @@
           </div>
 
           <div class="swiper-slide col-12 col-lg-6">
-            <div class="p-3 col d-flex">
-              <div class="post p-3 col-lg-12 d-lg-flex">
+            <div class="p-3 col d-flex h-100">
+              <div class="post p-3 d-lg-flex">
                 <div class="post-img col mb-2 mb-lg-0 col-lg-5">
                   <img loading="lazy" class="img-fluid" src="https://picsum.photos/800/500?random=1" alt="">
                 </div>
@@ -37,10 +37,10 @@
                     <p>
                       <strong>Quem foi Maharishi Mahesh Yogi, o fundador da Meditação Transcendental®</strong></p>
                     <p>
-                      Conheça o cientista Dr. Tony Nader, chefe da Organização Internacional de Meditação Transcendental®
+                    Conheça o cientista Dr. Tony Nader, chefe da Organização Internacional de Meditação Transcendental®Conheça o cientista Dr. Tony Nader, chefe da Organização Internacional de Meditação Transcendental®Conheça o cientista Dr. Tony Nader, chefe da Organização Internacional de Meditação Transcendental®
                     </p>
                   </div>
-                  <div class="item-btn d-flex justify-content-center justify-content-lg-end">
+                  <div class="item-btn d-flex align-items-end justify-content-center justify-content-lg-end">
                     <a class="btn visite-blog-btn text-uppercase" href="#" role="button">Leia +</a>
                   </div>
                 </div>
@@ -52,7 +52,7 @@
         </div>
         <div class="swiper-button-next"></div>
         <div class="swiper-button-prev"></div>
-        <div class="swiper-pagination"></div>
+        <!-- <div class="swiper-pagination"></div> -->
       </div>
     </div>
 </section>
@@ -62,7 +62,7 @@
       slidesPerView: 1,
       slidesPerGroup: 1,
       loop: true,
-      spaceBetween: 10,
+      spaceBetween: 0,
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
