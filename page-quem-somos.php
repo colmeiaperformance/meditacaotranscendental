ab24dd07b2ce397d21909e6af216dbab6c996158
Igor Fonseca <igor.stutzf@gmail.com>
2022-06-07 17:11:51 -0300

    update quem somos carousel

--- page-quem-somos.php
+++ page-quem-somos.php
@@ -223,10 +223,33 @@
 
           <div class="carousel-item text-center">
             <div class="container">
-              <img loading="lazy" src="<?php echo get_template_directory_uri() . '/images/quem-somos/Maharishi_International_University_logo.png' ?>" alt="MIU - Maharishi International University">
-              <h5><stromg>MIU - Maharishi International University</strong></h5>
-              <p>Fundada por Maharishi Mahesh Yogi, em 1971, em Iowa (EUA), é uma universidade que trabalha a educação superior com base na Consciência do Ser. O propósito da instituição é ajudar os alunos a desenvolverem um autoconhecimento profundo enquanto estudam o campo acadêmico de sua escolha. Oferece diversos cursos de graduação e pós-graduação.</p>
+              <img loading="lazy" src="<?php echo get_template_directory_uri() . '/images/quem-somos/meru.png' ?>" alt="MERU - Maharishi European Research University">
+              <h5><stromg>MERU - Maharishi European Research University</strong></h5>
+              <p>Fundada em 1975, na Suíça, por Maharishi Mahesh Yogi, para promover pesquisas sobre a Consciência. Em 1984, a MERU comprou um campus em uma floresta nacional perto de Vlodrop, na Holanda, onde Maharishi passou os últimos 18 anos de sua vida. É a sede da administração das organizações de MT e um anfitrião de cursos e retiros para as comunidades globais e locais. Sua missão é levar o conhecimento atemporal da Consciência a todos – para uma sociedade mais saudável, vibrante e pacífica.</p>
             </div>
+          </div>
+
+          <div class="carousel-item text-center">
+            <div class="container">
+              <img loading="lazy" src="<?php echo get_template_directory_uri() . '/images/quem-somos/umlac.png' ?>" alt="UMLAC - Universidade Maharishi da América Latina e Caribe">
+              <h5><stromg>UMLAC - Universidade Maharishi da América Latina e Caribe</strong></h5>
+              <p>A UMLAC oferece cursos de graduação, pós-graduação e Ph.D, especialmente na área da Saúde, tendo a Educação Baseada na Consciência como princípio fundamental para despertar o pleno potencial de criatividade, de inteligência e de felicidade em todos os estudantes e, assim, formar indivíduos para sociedades mais pacíficas e equilibradas. </p>
+            </div>
+          </div>
+
+          <div class="carousel-item text-center">
+            <div class="container">
+              <img loading="lazy" src="<?php echo get_template_directory_uri() . '/images/quem-somos/mii.png' ?>" alt="MII - Maharishi Invincibility Institute - África do Sul">
+              <h5><stromg>MII - Maharishi Invincibility Institute - África do Sul</strong></h5>
+              <p>Principal provedor de educação e treinamento privado sem fins lucrativos da África do Sul, a MII segue a metodologia da Educação Baseada na Consciência, incluindo a MT, para desenvolver o pleno potencial de cada aluno e com um modelo inovador de acesso à educação de ponta, com taxa de 95% de colocação profissional.</p>
+            </div>
+          </div>
+
+          <div class="carousel-item text-center">
+            <div class="container">
+              <img loading="lazy" src="<?php echo get_template_directory_uri() . '/images/quem-somos/david.png' ?>" alt="David Lynch Foundation ">
+              <h5><stromg>David Lynch Foundation </strong></h5>
+              <p>Fundada em 2005 pelo cineasta norte-americano David Lynch, um dos maiores entusiastas da MT no mundo, sua missão é erradicar a epidemia generalizada de trauma e de estresse tóxico entre populações em risco, através do ensino da MT, beneficiando milhares de crianças e adultos no mundo. No Brasil, os projetos são realizados pelo Instituto David Lynch (IDL).</div>
           </div>
 
         <button class="carousel-control-prev" type="button" data-bs-target="#organizacoes-mt" data-bs-slide="prev">
