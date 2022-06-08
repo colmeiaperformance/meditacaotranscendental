f73d3e8a84a5aeaef4a4a764b784ffeb1eae0305
Bruna Rafaela <brunarafaelav@outlook.com>
2022-06-08 15:49:09 -0300

    Update page-beneficios

--- page-quem-somos.php
+++ page-quem-somos.php
@@ -12,6 +12,7 @@
       <h2>Nosso propósito é criar uma sociedade mais <strong>pacífica e iluminada</strong> em todo o mundo, através do <strong>pleno desenvolvimento</strong> de cada indivíduo.</h2>
     </div>
   </section> -->
+  
   <section class="sobre-nos">
     <div class="container">
       <h2>Sobre <strong>nós</strong></h2>
--- page-sobre.php
+++
@@ -1,6 +0,0 @@
-<?php get_header(); ?>
-  <?php get_template_part('template-parts/navbar'); ?>
-<main class="page-sobre">
-  <?php get_template_part('template-parts/flexible-content'); ?>
-</main>
-<?php get_footer();
