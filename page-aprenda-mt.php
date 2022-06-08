6f8af20ee8deba5b843ca3e544fd3b54139da81f
Bruna Rafaela <brunarafaelav@outlook.com>
2022-06-08 16:38:50 -0300

    remove CTA encontre um instrutor credenciado page-aprenda-mt

--- page-aprenda-mt.php
+++ page-aprenda-mt.php
@@ -293,7 +293,6 @@
     <div class="container text-center">
       <h2>Dúvidas?</h2>
       <p>Veja as respostas para as  <a href="<?php echo home_url( '/faq/' ); ?>">perguntas mais frequentes.</a></p>
-      <a href="<?php echo home_url( '/instrutores/' ); ?>" class="btn" role="btn">Encontre um instrutor certificado</a>
     </div>
   </section>
 </main>
