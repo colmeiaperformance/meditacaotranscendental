3842220a9fd50243d873b3752d7e03c964af3485
Bruna Rafaela <brunarafaelav@outlook.com>
2022-06-08 16:34:42 -0300

    add link perguntas frequentes na page aprenda-mt

--- page-aprenda-mt.php
+++ page-aprenda-mt.php
@@ -292,7 +292,7 @@
   <section class="aprenda-mt-footer">
     <div class="container text-center">
       <h2>Dúvidas?</h2>
-      <p>Veja as respostas para as <a href="#">perguntas mais frequentes.</a></p>
+      <p>Veja as respostas para as  <a href="<?php echo home_url( '/faq/' ); ?>">perguntas mais frequentes.</a></p>
       <a href="<?php echo home_url( '/instrutores/' ); ?>" class="btn" role="btn">Encontre um instrutor certificado</a>
     </div>
   </section>
