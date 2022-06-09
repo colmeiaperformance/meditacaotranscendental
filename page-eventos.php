966df8ca203ad10247055cdb4439f1d829010a44
Bruna Rafaela <brunarafaelav@outlook.com>
2022-06-09 20:43:29 -0300

    Update page-eventos: remove seção footer

--- page-eventos.php
+++ page-eventos.php
@@ -11,13 +11,13 @@
             <?php endwhile; ?>
         <?php endif; ?>
     </div>
-<section class="eventos-footer">
+<!-- <section class="eventos-footer">
     <div class="container">
       <div>
         <img loading="lazy" class="img-fluid" src=<?php echo get_template_directory_uri() . '/images/instrutores/mt.png' ?>
             alt="mt."><h2>Aprenda a meditar de forma <Strong>simples e fácil</Strong>.</h2>
       </div>
     </div>
-</section>    
+</section>     -->
 </main>
 <?php get_footer();
