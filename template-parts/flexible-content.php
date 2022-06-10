7ba0bea3254902f3c2bf3cd2fb235319229908c2
Becky Marques <becky.marques@colmeiaperformance.com.br>
2022-06-10 15:27:50 -0300

    Update flexible-content.php

--- template-parts/flexible-content.php
+++ template-parts/flexible-content.php
@@ -1,3 +1,85 @@
+<?php
+
+// Check value exists.
+if( have_rows('content') ):
+
+  // Loop through rows.
+  while ( have_rows('content') ) : the_row(); ?>
+
+<?php
+    // Section: Home - MT Unica.
+    if( get_row_layout() == 'mt_unica' ): ?>
+<?php get_template_part('template-parts/sections/home/mt-unica/mt-unica'); ?>
+
+
+<?php
+    // Section: Home - MT Unica - Banners.
+    elseif( get_row_layout() == 'mt_unica_banners' ): ?>
+<?php get_template_part('template-parts/sections/home/mt-unica/mt-unica-slide'); ?>
+
+<?php
+    // Section: Home - MT Unica - Depoimentos.
+    elseif( get_row_layout() == 'mt_unica_depoimentos' ): ?>
+<?php get_template_part('template-parts/sections/home/mt-unica/mt-unica-depoimentos'); ?>
+
+<?php
+    // Section: Home - Beneficios da MT.
+    elseif( get_row_layout() == 'beneficios_da_mt' ): ?>
+<?php get_template_part('template-parts/sections/home/conheca-beneficios'); ?>
+
+
+<?php
+    // Section: Home - Como Aprender MT.
+    elseif( get_row_layout() == 'como_aprender_mt' ): ?>
+<?php get_template_part('template-parts/sections/home/como-aprender'); ?>
+
+<?php
+    // Section: Home - O Que Dizem.
+    elseif( get_row_layout() == 'o_que_dizem' ): ?>
+<?php get_template_part('template-parts/sections/pages/o-que-dizem'); ?>
+
+<?php
+    // Section: Home - Transcender.
+    elseif( get_row_layout() == 'transcender' ): ?>
+<?php get_template_part('template-parts/sections/home/transcender'); ?>
+
+<?php
+    // Section: Home - Mundo Melhor.
+    elseif( get_row_layout() == 'para_um_mundo_melhor' ): ?>
+<?php get_template_part('template-parts/sections/home/mundo-melhor'); ?>
+
+<?php
+    // Section: Home - Visite Nosso Blog.
+    elseif( get_row_layout() == 'visite_nosso_blog' ): ?>
+<?php get_template_part('template-parts/sections/home/visite-blog'); ?>
+
+<?php
+    // Section: Home - Agende Palestra.
+    elseif( get_row_layout() == 'agende_palestra' ): ?>
+<?php get_template_part('template-parts/sections/home/agende-palestra'); ?>
+
+<?php
+        // Section: Section Name.
+        // if( get_row_layout() == 'paragraph' ):
+        //     $text = get_sub_field('text');
+            // Do something...
+
+        // Section: Section Name.
+        // elseif( get_row_layout() == 'download' ): 
+        //     $file = get_sub_field('file');
+            // Do something...
+
+        endif;
+
+    // End loop.
+    endwhile;
+
+// No value.
+else :
+    // Do something...
+endif;
+?>
+
 <?php get_template_part('template-parts/sections/home/mt-unica/mt-unica'); ?>
 <?php get_template_part('template-parts/sections/home/mt-unica/mt-unica-slide'); ?>
 <?php get_template_part('template-parts/sections/home/mt-unica/mt-unica-depoimentos'); ?>
