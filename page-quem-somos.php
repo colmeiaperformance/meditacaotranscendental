d75431f527cd779f168e9eafbdc02da9fe0f1fa9
Igor Fonseca <igor.stutzf@gmail.com>
2022-06-13 16:30:43 -0300

    update margins

--- page-quem-somos.php
+++ page-quem-somos.php
@@ -16,9 +16,11 @@
   <section class="sobre-nos">
     <div class="container">
       <h2>Sobre <strong>nós</strong></h2>
-      <p>Somos uma organização mundial sem fins lucrativos, presente em mais de 100 países, que tem como objetivo levar o programa da Meditação Transcendental a todas as pessoas – bem como para governos, empresas e escolas. Por meio de milhares de instrutores em todo o mundo, já ensinamos a MT a mais de 10 milhões de pessoas, de diferentes culturas, crenças e estilos de vida.</p>
-      <p>Desde 2008, o Dr. Tony Nader é o líder global da MT – ele foi escolhido por Maharishi como seu sucessor. Médico por Harvard e Ph.D em Neurociências pelo Massachusetts Institute of Technology (MIT), é um grande cientista e estudioso védico, autor de muitas publicações sobre a relação da mente, da Consciência e da fisiologia.</p>
-      <p><strong>No Brasil, atuamos através da Associação Internacional de Meditação (SIM).</strong></p>
+      <div class="mx-lg-5 px-lg-5">
+        <p>Somos uma organização mundial sem fins lucrativos, presente em mais de 100 países, que tem como objetivo levar o programa da Meditação Transcendental a todas as pessoas – bem como para governos, empresas e escolas. Por meio de milhares de instrutores em todo o mundo, já ensinamos a MT a mais de 10 milhões de pessoas, de diferentes culturas, crenças e estilos de vida.</p>
+        <p>Desde 2008, o Dr. Tony Nader é o líder global da MT – ele foi escolhido por Maharishi como seu sucessor. Médico por Harvard e Ph.D em Neurociências pelo Massachusetts Institute of Technology (MIT), é um grande cientista e estudioso védico, autor de muitas publicações sobre a relação da mente, da Consciência e da fisiologia.</p>
+        <p><strong>No Brasil, atuamos através da Associação Internacional de Meditação (SIM).</strong></p>
+      </div>
     </div>
   </section>
   <section class="projetos">
@@ -124,7 +126,7 @@
   </section>
   <section class="organizacoes">
     <div class="container">
-      <div class="head">
+      <div class="head mx-lg-5 px-lg-5">
         <h2><strong>Organizações MT</strong> no mundo</h2>
         <p>A Meditação Transcendental é amplamente difundida e possui seus benefícios propagados por meio de diversas organizações em todo o mundo.</p>
       </div>
