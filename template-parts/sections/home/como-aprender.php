20d764b747451dd8ba3b79d184b0fd553407439e
Becky Marques <becky.marques@colmeiaperformance.com.br>
2022-07-11 15:47:55 -0300

    Add ACF Como aprender MT

--- template-parts/sections/home/como-aprender.php
+++ template-parts/sections/home/como-aprender.php
@@ -1,34 +1,51 @@
+<?php 
+$titulo_camt = get_sub_field('titulo_camt');
+$logo_camt = get_sub_field('logo_camt');
+$texto_camt = get_sub_field('texto_camt');
+$lista_camt = get_sub_field('lista_camt');
+$texto_do_cta_camt = get_sub_field('texto_do_cta_camt');
+$texto_do_botao_camt = get_sub_field('texto_do_botao_camt');
+$link_do_botao_camt = get_sub_field('link_do_botao_camt');
+?>
+
 <section class="como-aprender py-5">
   <div class="container">
-    <h2 class="como-aprender-title mb-0 mb-lg-5">Como aprender <strong><span  class="yellow">MT</span></strong> ?</h2>
+    <h2 class="como-aprender-title mb-0 mb-lg-5">
+      <?php if ( $titulo_camt ) { echo $titulo_camt; } else { echo ''; } ?>
+    </h2>
     <div class="row align-items-center justify-content-center">
 
       <div class="col-12 col-lg-6 d-none d-lg-block d-lg-flex">
         <div class="col-12 col-lg-4 d-flex align-items-start justify-content-center">
-          <img class="img-fluid como-aprender-img" src=<?php echo get_template_directory_uri() . '/images/aprenda-mt/icon-arvore.png' ?>
-            alt="Aprenda MT">
+          <img class="img-fluid como-aprender-img" src=<?php if ( $logo_camt['url'] ) { echo $logo_camt['url']; } else { echo ''; } ?>
+            alt="<?php if ( $logo_camt['alt'] ) { echo $logo_camt['alt']; } else { echo ''; } ?>">
         </div>
         <div class="col-12 col-lg-8 como-aprender-texto mb-4">
-          <p>A Meditação Transcendental é ensinada somente por professores credenciados pela Associação Internacional de
-            Meditação (SIM).</p>
-          <p>Mais que um curso. O início de uma jornada de autoconhecimento e evolução.</p>
+          <?php if ( $texto_camt ) { echo $texto_camt; } else { echo ''; } ?>
         </div>
       </div>
 
       <div class="row text-call mt-0 mt-lg-4 d-block d-lg-none text-center">
         <p>
-          O <strong>primeiro passo</strong> para aprender é participar de uma <strong><span class="yellow">palestra online gratuita</span></strong>.
+          <?php if ( $texto_do_cta_camt ) { echo $texto_do_cta_camt; } else { echo ''; } ?>
         </p>
       </div>
 
       <div class="col-12 col-lg-6">
         <div class="como-aprender-box">
+
+          <?php if ($lista_camt) { ?>
           <ul class="como-aprender-list">
-            <li class="como-aprender-list-item">A Meditação Transcendental é ensinada apenas por instrutores certificados e credenciados</li>
-            <li class="como-aprender-list-item">O curso é realizado em 4 encontros presenciais.</li>
-            <li class="como-aprender-list-item">Você aprende a meditar através da experiência prática com a MT.</li>
-            <li class="como-aprender-list-item">Após o curso, você terá acompanhamento sempre que precisar.</li>
+
+            <?php foreach ($lista_camt as $l_camt) { ?>
+            
+            <li class="como-aprender-list-item"><?php if( $l_camt['item'] ) { echo $l_camt['item']; } else { echo ''; } ?></li>
+
+            <?php } ?>
+
           </ul>
+          <?php } ?>
+
         </div>
 
       </div>
@@ -37,13 +54,13 @@
 
     <div class="row text-call mt-sm-4 d-none d-lg-block text-center">
       <p>
-        O <strong>primeiro passo</strong> para aprender é participar de uma <strong><span class="yellow">palestra online gratuita</span></strong>.
+        <?php if ( $texto_do_cta_camt ) { echo $texto_do_cta_camt; } else { echo ''; } ?>
       </p>
     </div>
 
     <div class="row justify-content-center mt-4 mt-lg-0">
       <div class="col-12 text-center">
-        <a class="btn px-4" href="<?php echo home_url( '/agendamento/' ); ?>" role="button">Inscreva-se</a>
+        <a class="btn px-4" href="<?php if ( $link_do_botao_camt ) { echo $link_do_botao_camt; } else { echo ''; } ?>" role="button"><?php if ( $texto_do_botao_camt ) { echo $texto_do_botao_camt; } else { echo ''; } ?></a>
       </div>
     </div>
   </div>
