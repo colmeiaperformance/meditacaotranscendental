893839d41e27500e312d6581f1620b9aaa429a91
Becky Marques <becky.marques@colmeiaperformance.com.br>
2022-07-11 12:42:18 -0300

    Add ACF Beneficios MT Home

--- template-parts/flexible-content.php
+++ template-parts/flexible-content.php
@@ -85,7 +85,7 @@ endif;
 ?>
 <?php //get_template_part('template-parts/sections/home/mt-unica/mt-unica-slide'); ?>
 <?php // get_template_part('template-parts/sections/home/mt-unica/mt-unica-depoimentos'); ?>
-<?php get_template_part('template-parts/sections/home/conheca-beneficios'); ?>
+<?php //get_template_part('template-parts/sections/home/conheca-beneficios'); ?>
 <?php get_template_part('template-parts/sections/home/como-aprender'); ?>
 <?php get_template_part('template-parts/sections/pages/o-que-dizem'); ?>
 <?php get_template_part('template-parts/sections/home/transcender'); ?>
--- template-parts/sections/home/conheca-beneficios.php
+++ template-parts/sections/home/conheca-beneficios.php
@@ -1,85 +1,44 @@
+<?php
+$titulo_bmt = get_sub_field('titulo_bmt');
+$cards_bmt = get_sub_field('cards_bmt');
+$citacao_bmt = get_sub_field('citacao_bmt');
+$autor_bmt = get_sub_field('autor_bmt');
+$cor_de_fundo_bmt = get_sub_field('cor_de_fundo_bmt');
+?>
 <section class="conheca-beneficios">
   <div class="container">
-    <h2>Conheça os <strong>benefícios</strong> da MT</h2>
+    <h2><?php if ( $titulo_bmt ) { echo $titulo_bmt; } else { echo ''; } ?></h2>
     <div class="row align-items-center justify-content-center">
+
+      <?php foreach ($cards_bmt as $c_bmt) { ?>
+      
       <div class="col-12 col-lg-3 d-flex align-items-center justify-content-center p-0">
         <div class="box rounded-circle text-center">
           <div class="default">
-              <img loading="lazy" class="img-fluid" src=<?php echo get_template_directory_uri() . '/images/beneficios/mental.png' ?>
-            alt="Benefícios: Saúde Mental">
-              <h4>Saúde Mental</h4>
+              <img loading="lazy" class="img-fluid" src=<?php if ( $c_bmt['logo_card']['url'] ) { echo $c_bmt['logo_card']['url']; } else { echo ''; } ?>
+            alt="<?php if ( $c_bmt['logo_card']['alt'] ) { echo $c_bmt['logo_card']['alt']; } else { echo ''; } ?>">
+              <h4><?php if ( $c_bmt['titulo_card'] ) { echo $c_bmt['titulo_card']; } else { echo ''; } ?></h4>
               <span>+</span>
           </div>
            <div class="hover">
             <p>
-              Reduz ansiedade, depressão, síndrome do pânico e burnout. Promove paz interior, autoconfiança e felicidade.
+              <?php if ( $c_bmt['descricao_card'] ) { echo $c_bmt['descricao_card']; } else { echo ''; } ?>
             </p>
-            <a href="<?php echo home_url( '/beneficios/' ); ?>" class="btn">
-              + Saiba mais
+            <a href="<?php if ( $c_bmt['link_do_botao_card'] ) { echo $c_bmt['link_do_botao_card']; } else { echo ''; } ?>" class="btn">
+              <?php if ( $c_bmt['texto_do_botao_card'] ) { echo $c_bmt['texto_do_botao_card']; } else { echo ''; } ?>
             </a>
           </div>
         </div>
       </div>
-      <div class="col-12 col-lg-3 d-flex align-items-center justify-content-center p-0">
-         <div class="box rounded-circle text-center">
-          <div class="default">
-              <img loading="lazy" class="img-fluid" src=<?php echo get_template_directory_uri() . '/images/beneficios/fisica.png' ?>
-              alt="Benefícios: Saúde Mental">
-            <h4>Saúde Física</h4>
-            <span>+</span>
-          </div>
-           <div class="hover">
-            <p>
-              Melhora a qualidade do sono, frequência cardíaca, pressão arterial e outros indicadores de saúde. 
-            </p>
-            <a href="<?php echo home_url( '/beneficios/' ); ?>" class="btn">
-              + Saiba mais
-            </a>
-          </div>
-        </div>
-      </div>
-      <div class="col-12 col-lg-3 d-flex align-items-center justify-content-center p-0">
-         <div class="box rounded-circle text-center">
-          <div class="default">
-            <img loading="lazy" class="img-fluid" src=<?php echo get_template_directory_uri() . '/images/beneficios/emocional.png' ?>
-              alt="Benefícios: Saúde Mental">
-            <h4>Inteligência emocional</h4>
-            <span>+</span>
-          </div>
-          <div class="hover">
-            <p>
-              Promove empatia, melhora a qualidade dos relacionamentos e leva à autoreealização.
-            </p>
-            <a href="<?php echo home_url( '/beneficios/' ); ?>" class="btn">
-              + Saiba mais
-            </a>
-          </div>
-        </div>
-      </div>
-      <div class="col-12 col-lg-3 d-flex align-items-center justify-content-center p-0">
-         <div class="box box-last rounded-circle text-center">
-          <div class="default">
-              <img loading="lazy" class="img-fluid" src=<?php echo get_template_directory_uri() . '/images/beneficios/potencia.png' ?>
-            alt="Benefícios: Saúde Mental">
-            <h4>Potencial Mental</h4>
-            <span>+</span>
-            </div>
-             <div class="hover">
-            <p>
-              Aumenta o foco, criatividade e clareza mental. Melhora a memória, percepção e capacidade de resolver problemas.
-            </p>
-            <a href="<?php echo home_url( '/beneficios/' ); ?>" class="btn">
-              + Saiba mais
-            </a>
-          </div>
-        </div>
-      </div>
+
+      <?php } ?>
+
     </div>
     <div class="row">
       <div class="col-12 quote">
         <div>
-          <p>O propósito da vida é a <strong>expansão da felicidade</strong></p>
-          <h4>Maharishi Mahesh Yogi</h4>
+          <p><?php if ( $citacao_bmt ) { echo $citacao_bmt; } else { echo ''; } ?></p>
+          <h4><?php if ( $autor_bmt ) { echo $autor_bmt; } else { echo ''; } ?></h4>
         </div>
     </div>
     </div>
