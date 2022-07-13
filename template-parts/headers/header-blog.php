23c251fd99d12fb1e54ade5453c826ec7551229e
Bruna Rafaela <brunarafaelav@outlook.com>
2022-07-13 00:20:16 -0300

    Update single, blog

--- template-parts/headers/header-blog.php
+++ template-parts/headers/header-blog.php
@@ -1,5 +1,17 @@
-<?php get_template_part('template-parts/breadcrumb'); ?>
+<section id="breadcrumb" class="breadcrumb">
+  <div class="container">
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="/">Home</a></li>
+        <li class="breadcrumb-item">Blog</li>
+      </ol>
+    </nav>
+  </div>
+</section>
 <header class="header--blog">
   <div class="container position-relative overflow-hidden">
+    <div class="col p-lg-5 text-center">
+      <h1>Blog</h1>
+    </div>
   </div>
 </header>
