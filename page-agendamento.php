6ad997477b9fc313a1f5f56d07fa13ed0b5c724b
Bruna Rafaela <brunarafaelav@outlook.com>
2022-08-17 00:55:54 -0300

    Update scss

--- page-agendamento.php
+++ page-agendamento.php
@@ -5,7 +5,7 @@
     <div class="container">
       <h2>Inscreva-se na <br /><strong>palestra online e gratuita</strong></h2>
       <p>Selecione abaixo seu Estado e Cidade e veja as datas de sua preferência!<br> 
-A palestra inicial é online e gratuita, assim você pode esclarecer suas dúvidas e entender porque a MT é única. O curso será presencial.</strong>.</p>
+      A palestra inicial é online e gratuita, assim você pode esclarecer suas dúvidas e entender porque a MT é única. O curso será presencial.</p>
     </div>
   </section>
   <section class="">
