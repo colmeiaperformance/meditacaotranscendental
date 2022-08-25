f115cd8748fa24527ed09c77a14c1e8dbba7d256
Becky Marques <becky.marques@colmeiaperformance.com.br>
2022-08-25 14:21:32 -0300

    [Add] ACF Mundo Melhor Home

--- template-parts/flexible-content.php
+++ template-parts/flexible-content.php
@@ -45,7 +45,7 @@ if (have_rows('content')):
 <?php get_template_part('template-parts/sections/home/transcender'); ?>
 
 <?php
-    // Section: Home - Mundo Melhor.
+    // Section: Home - Mundo Melhor. ACF OK
     elseif (get_row_layout() == 'para_um_mundo_melhor'): ?>
 <?php get_template_part('template-parts/sections/home/mundo-melhor'); ?>
 
@@ -89,6 +89,6 @@ endif;
 <?php //get_template_part('template-parts/sections/home/como-aprender'); ?>
 <?php //get_template_part('template-parts/sections/pages/o-que-dizem'); ?>
 <?php //get_template_part('template-parts/sections/home/transcender'); ?>
-<?php get_template_part('template-parts/sections/home/mundo-melhor'); ?>
+<?php //get_template_part('template-parts/sections/home/mundo-melhor'); ?>
 <?php /* get_template_part('template-parts/sections/home/visite-blog'); */?>
 <?php get_template_part('template-parts/sections/home/agende-palestra'); ?>
--- template-parts/sections/home/mundo-melhor.php
+++ template-parts/sections/home/mundo-melhor.php
@@ -1,21 +1,31 @@
-<section class="mundo-melhor position-relative">
+<?php
+$cor_de_fundo_mm = get_sub_field('cor_de_fundo_mm');
+$image_mobile_mm = get_sub_field('image_mobile_mm');
+$logo_mm = get_sub_field('logo_mm');
+$titulo_mm = get_sub_field('titulo_mm');
+$texto_mm = get_sub_field('texto_mm');
+$link_do_botao_mm = get_sub_field('link_do_botao_mm');
+$texto_do_botao_mm = get_sub_field('texto_do_botao_mm');
+$image_desktop_mm = get_sub_field('image_desktop_mm');
+?>
+
+<section class="mundo-melhor position-relative" style="background-color: <?php if ( $cor_de_fundo_mm ) { echo $cor_de_fundo_mm; } else { echo ''; } ?>;">
   <div class="mundo-melhor-topo">
-    <img loading="lazy" src="<?php echo get_template_directory_uri() . '/images/mundo-melhor/mundo-melhor-topo.png' ?>" alt="mt.">
+    <img loading="lazy" src="<?php if ( $image_mobile_mm ) { echo $image_mobile_mm; } else { echo ''; } ?>" alt="mt.">
   </div>
   <div class="container">
     <div class="row align-items-center justify-content-start">
       <div class="col-12 col-lg-7">
        <div class="text-center text-lg-start">
-          <h2><img loading="lazy" src=<?php echo get_template_directory_uri() . '/images/mundo-melhor/mt-mundo-melhor.png' ?> alt="mt."></h2>
-         <h3>para um mundo <strong>melhor</strong></h3>
+       <?php if ( $logo_mm ) { echo '<h2><img loading="lazy" src="' . $logo_mm . '" alt="mt."></h2>'; } else { echo ''; } ?>
+         <h3><?php if ( $titulo_mm ) { echo $titulo_mm; } else { echo ''; } ?></h3>
        </div>
-        <p>Cada indivíduo é uma célula do tecido social. Um indivíduo feliz e realizado se torna uma peça na construção de um mundo melhor. </p>
-        <p><strong>Maharishi Mahesh Yogi</strong>, fundador da Meditação Transcendental ®, assumiu a missão de levar a prática da MT ao maior número possível de pessoas e, assim, criar uma sociedade global mais pacífica por meio do pleno desenvolvimento de cada ser humano.</p>
+       <?php if ( $texto_mm ) { echo $texto_mm; } else { echo ''; } ?>
         <div class="text-center text-lg-start">
-          <a href="<?php echo home_url( '/quem-somos/' ); ?>" class="btn">Conheça nosso propósito</a>
+          <a href="<?php if ( $link_do_botao_mm ) { echo $link_do_botao_mm; } else { echo ''; } ?>" class="btn"><?php if ( $texto_do_botao_mm ) { echo $texto_do_botao_mm; } else { echo ''; } ?></a>
         </div>
       </div>
     </div>
-    <div class="mundo-melhor-bg position-absolute"></div>
+    <div class="mundo-melhor-bg position-absolute" style="background: url(<?php if ( $image_desktop_mm ) { echo $image_desktop_mm; } else { echo ''; } ?>); background-size: cover;"></div>
   </div>
 </section>
