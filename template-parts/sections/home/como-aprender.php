5805a5219ae8c86323da5a56c0b3c6381c6d8a12
Becky Marques <becky.marques@colmeiaperformance.com.br>
2022-09-19 14:40:08 -0300

    [Update] Adjust home como-aprender beneficios cta

--- template-parts/sections/home/como-aprender.php
+++ template-parts/sections/home/como-aprender.php
@@ -43,7 +43,7 @@
 
     <div class="row justify-content-center mt-4 mt-lg-0">
       <div class="col-12 text-center">
-        <a class="btn px-4" href="<?php echo home_url( '/agendamento/' ); ?>" role="button">Inscreva-se</a>
+        <a class="btn px-4" href="<?php echo home_url( '/agendamento/' ); ?>" role="button">Inscreva-se na Palestra Gratuita</a>
       </div>
     </div>
   </div>
