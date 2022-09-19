5fe15b3cd42442039176f425bebeeebf06f705a6
Becky Marques <becky.marques@colmeiaperformance.com.br>
2022-09-19 15:12:30 -0300

    [Fix] Position depoimentos

--- template-parts/flexible-content.php
+++ template-parts/flexible-content.php
@@ -1,8 +1,8 @@
 <?php get_template_part('template-parts/sections/home/mt-unica/mt-unica'); ?>
 <?php get_template_part('template-parts/sections/home/mt-unica/mt-unica-slide'); ?>
 <?php get_template_part('template-parts/sections/home/como-aprender'); ?>
-<?php get_template_part('template-parts/sections/home/mt-unica/mt-unica-depoimentos'); ?>
 <?php get_template_part('template-parts/sections/home/conheca-beneficios'); ?>
+<?php get_template_part('template-parts/sections/home/mt-unica/mt-unica-depoimentos'); ?>
 <?php get_template_part('template-parts/sections/pages/o-que-dizem'); ?>
 <?php get_template_part('template-parts/sections/home/transcender'); ?>
 <?php get_template_part('template-parts/sections/home/mundo-melhor'); ?>
