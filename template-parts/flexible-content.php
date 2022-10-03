7f6421febc31dd5fe4b4c6e66a38e5144cbde102
Bruna Rafaela <brunarafaelav@outlook.com>
2022-10-03 18:31:47 -0300

    Update pages templates

--- template-parts/flexible-content.php
+++ template-parts/flexible-content.php
@@ -1,10 +1,10 @@
-<?php get_template_part('template-parts/sections/home/mt-unica/mt-unica'); ?>
-<?php get_template_part('template-parts/sections/home/mt-unica/mt-unica-slide'); ?>
-<?php get_template_part('template-parts/sections/home/como-aprender'); ?>
-<?php get_template_part('template-parts/sections/home/conheca-beneficios'); ?>
-<?php get_template_part('template-parts/sections/home/mt-unica/mt-unica-depoimentos'); ?>
-<?php get_template_part('template-parts/sections/pages/o-que-dizem'); ?>
-<?php get_template_part('template-parts/sections/home/transcender'); ?>
-<?php get_template_part('template-parts/sections/home/mundo-melhor'); ?>
+<?php get_template_part('template-parts/pages/home/mt-unica/mt-unica'); ?>
+<?php get_template_part('template-parts/pages/home/mt-unica/mt-unica-slide'); ?>
+<?php get_template_part('template-parts/pages/home/como-aprender'); ?>
+<?php get_template_part('template-parts/pages/home/conheca-beneficios'); ?>
+<?php get_template_part('template-parts/pages/home/mt-unica/mt-unica-depoimentos'); ?>
+<?php get_template_part('template-parts/pages/pages/o-que-dizem'); ?>
+<?php get_template_part('template-parts/pages/home/transcender'); ?>
+<?php get_template_part('template-parts/pages/home/mundo-melhor'); ?>
 <?php /* get_template_part('template-parts/sections/home/visite-blog'); */ ?>
-<?php get_template_part('template-parts/sections/home/agende-palestra'); ?>
+<?php get_template_part('template-parts/pages/home/agende-palestra'); ?>
