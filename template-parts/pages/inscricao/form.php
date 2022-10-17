2d7292f5c6e91d7e9b6fc3b66bab3a23899fe747
Bruna Rafaela <brunarafaelav@outlook.com>
2022-10-17 06:27:38 -0300

    update moved template sections inscricao-form and pesquisas-cientificas to sections foler

--- template-parts/pages/inscricao/form.php
+++ template-parts/pages/inscricao/form.php
@@ -1,5 +1,5 @@
 <section class="inscricao-form">
     <div class="container">
-      <?php get_template_part('template-parts/sections/pages/inscricao-form'); ?>
+      <?php get_template_part('template-parts/sections/inscricao-form'); ?>
     </div>
   </section>
