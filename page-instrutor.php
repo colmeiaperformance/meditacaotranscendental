423ecd6860631ac9daecff05094cf9d0363c440f
Bruna Rafaela <brunarafaelav@outlook.com>
2022-11-09 17:33:08 -0300

    update page-instrutor.scss

--- page-instrutor.php
+++ page-instrutor.php
@@ -6,32 +6,33 @@
 </main>
 <style>
   #melhorPeriodo,
-#melhorDia {
-  width: 100%;
-  font-weight: 500 !important;
-  font-size: 1rem !important;
-  color: #F29F05 !important;
-  border: 1px solid #F29F05 !important;
-  border-radius: 30px !important;
-  text-align: left !important;
-  background: url("../resources/images/dropdown.png") !important;
-  background-position: right 0.75rem center !important;
-  background-color: #fff !important;
-  background-repeat: no-repeat !important;
-}
-#melhorPeriodo:active, #melhorPeriodo:focus,
-#melhorDia:active,
-#melhorDia:focus {
-  box-shadow: none !important;
-}
-@media (max-width: 992px) {
-  #melhorPeriodo,
-#melhorDia {
+  #melhorDia {
+    width: 100%;
+    font-weight: 500 !important;
     font-size: 1rem !important;
-    line-height: 18px !important;
-    padding: 7px 17px !important;
-    margin: 0 auto;
+    color: #F29F05 !important;
+    border: 1px solid #F29F05 !important;
+    border-radius: 30px !important;
+    text-align: left !important;
+    background: url("../resources/images/dropdown.png") !important;
+    background-position: right 0.75rem center !important;
+    background-color: #fff !important;
+    background-repeat: no-repeat !important;
   }
-}
+  #melhorPeriodo:active, 
+  #melhorPeriodo:focus,
+  #melhorDia:active,
+  #melhorDia:focus {
+    box-shadow: none !important;
+  }
+  @media (max-width: 992px) {
+    #melhorPeriodo,
+    #melhorDia {
+        font-size: 1rem !important;
+        line-height: 18px !important;
+        padding: 7px 17px !important;
+        margin: 0 auto;
+      }
+  }
 </style>
 <?php get_footer();
