d9d2842536e22e5d347592008c98bd0ccd714b6f
Bruna Rafaela <brunarafaelav@outlook.com>
2022-11-09 17:43:19 -0300

    update page-instrutor

--- page-instrutor.php
+++ page-instrutor.php
@@ -4,35 +4,4 @@
   <?php echo do_shortcode( '[mt_render_employee]' ); ?>
   <?php get_template_part('template-parts/pages/instrutor/footer'); ?>
 </main>
-<style>
-  #melhorPeriodo,
-  #melhorDia {
-    width: 100%;
-    font-weight: 500 !important;
-    font-size: 1rem !important;
-    color: #F29F05 !important;
-    border: 1px solid #F29F05 !important;
-    border-radius: 30px !important;
-    text-align: left !important;
-    background: url("images/instrutores/dropdown.png") !important;
-    background-position: right 0.75rem center !important;
-    background-color: #fff !important;
-    background-repeat: no-repeat !important;
-  }
-  #melhorPeriodo:active, 
-  #melhorPeriodo:focus,
-  #melhorDia:active,
-  #melhorDia:focus {
-    box-shadow: none !important;
-  }
-  @media (max-width: 992px) {
-    #melhorPeriodo,
-    #melhorDia {
-        font-size: 1rem !important;
-        line-height: 18px !important;
-        padding: 7px 17px !important;
-        margin: 0 auto;
-      }
-  }
-</style>
 <?php get_footer();
