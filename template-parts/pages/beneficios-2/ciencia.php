42b0d749b0109db1124d1aeed6c785894d146b5e
Bruna Rafaela <brunarafaelav@outlook.com>
2022-11-16 06:33:04 -0300

    Create page-beneficios-2 + sections

---
+++ template-parts/pages/beneficios-2/ciencia.php
@@ -0,0 +1,333 @@
+<section class="ciencia">
+    <div class="container">
+      <h2>O que diz a <strong>ciência</strong>?</h2>
+      <h3>A MT é 2x mais eficaz na redução da ansiedade do que qualquer outra técnica. (Resultado da meta análise de 146 estudos científicos)</h3>
+      <div class="list-group w-auto">
+        <div class="list-group-item d-flex align-items-center justify-content-center">
+          <img loading="lazy" class="rounded-circle border-0"
+          src=<?php echo get_template_directory_uri() . '/images/beneficios/mundo.png' ?>
+          alt="Ícone Mundo">
+          <p class="mb-0">Mais de 10 milhões de praticantes — de diferentes países, crenças, religiões, filosofias e estilos</p>
+        </div>
+        <div class="list-group-item d-flex align-items-center justify-content-center">
+          <img loading="lazy" class="rounded-circle border-0"
+          src=<?php echo get_template_directory_uri() . '/images/beneficios/lupa.png' ?>
+          alt="Ícone Mundo">
+          <p class="mb-0">Mais de 600 pesquisas científicas comprovam os benefícios</p>
+        </div>
+      </div>
+      <div class="swiper swiperCiencia">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide active">
+            <div class="swiper-mobile">
+              <div class="container px-sm-5">
+                <div class="row d-flex align-items-center justify-content-center px-3 px-sm-5">
+                  <div class="col-12 col-lg-5 order-2 order-lg-1">
+                    <h3>Burnout, insônia e depressão</h3>
+                    <div class="text-center d-block d-lg-none">
+                      <img loading="lazy" class="border-0"
+                        src=<?php echo get_template_directory_uri() . '/images/beneficios-mt/line.png' ?>
+                        alt="Linha">
+                    </div>
+                    <p>Durante a pandemia da covid-19, uma pesquisa mostrou redução do burnout, da depressão e da
+                      insônia em médicos que praticavam, regularmente, a MT.
+                    </p>
+                  </div>
+                  <div class="col-12 col-lg-7 d-flex align-items-center justify-content-center justify-content-lg-end p-0 mb-4 mb-lg-0 order-1 order-lg-2">
+                    <img loading="lazy" class="img-fluid"
+                      src=<?php echo get_template_directory_uri() . '/images/beneficios-mt/1.png' ?>
+                      alt="Gráfico">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="swiper-mobile">
+              <div class="container px-sm-5">
+                <div class="row d-flex align-items-center justify-content-center px-3 px-sm-5">
+                  <div class="col-12 col-lg-5 order-2 order-lg-1">
+                    <h3>Estresse</h3>
+                    <div class="text-center d-block d-lg-none">
+                      <img loading="lazy" class="border-0"
+                        src=<?php echo get_template_directory_uri() . '/images/beneficios-mt/line.png' ?>
+                        alt="Linha">
+                    </div>
+                    <p>Um outro estudo realizado durante a pandemia mostrou a redução de sintomas de estresse pós-traumático entre médicos da emergência do Hospital Presibiteriano de Nova Iorque.
+                    </p>
+                  </div>
+                  <div class="col-12 col-lg-7 d-flex align-items-center justify-content-center justify-content-lg-end p-0 mb-4 mb-lg-0 order-1 order-lg-2">
+                    <img loading="lazy" class="img-fluid"
+                      src=<?php echo get_template_directory_uri() . '/images/beneficios-mt/2.png' ?>
+                      alt="Gráfico">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="swiper-mobile">
+              <div class="container px-sm-5">
+                <div class="row d-flex align-items-center justify-content-center px-3 px-sm-5">
+                  <div class="col-12 col-lg-5 order-2 order-lg-1">
+                    <h3>Ansiedade</h3>
+                    <div class="text-center d-block d-lg-none">
+                      <img loading="lazy" class="border-0"
+                        src=<?php echo get_template_directory_uri() . '/images/beneficios-mt/line.png' ?>
+                        alt="Linha">
+                    </div>
+                    <p>Uma metanálise da Universidade de Stanford revisou 146 estudos científicos sobre os efeitos de diferentes técnicas para redução da ansiedade.
+                    </p>
+                    <p>Foi constatado que a MT é duas vezes mais eficaz do que qualquer outro tipo de meditação ou prática de relaxamento.
+                    </p>
+                  </div>
+                  <div class="col-12 col-lg-7 d-flex align-items-center justify-content-center justify-content-lg-end p-0 mb-4 mb-lg-0 order-1 order-lg-2">
+                    <img loading="lazy" class="img-fluid"
+                      src=<?php echo get_template_directory_uri() . '/images/beneficios-mt/3.png' ?>
+                      alt="Gráfico">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="swiper-mobile">
+              <div class="container px-sm-5">
+                <div class="row d-flex align-items-center justify-content-center px-3 px-sm-5">
+                  <div class="col-12 col-lg-5 order-2 order-lg-1">
+                    <h3>Insônia</h3>
+                    <div class="text-center d-block d-lg-none">
+                      <img loading="lazy" class="border-0"
+                        src=<?php echo get_template_directory_uri() . '/images/beneficios-mt/line.png' ?>
+                        alt="Linha">
+                    </div>
+                    <p>Depois de 90 dias de prática regular de MT, pacientes que foram observados diariamente diminuíram o tempo para caírem no sono de cerca de 1h15 para apenas 15 minutos.
+                    </p>
+                  </div>
+                  <div class="col-12 col-lg-7 d-flex align-items-center justify-content-center justify-content-lg-end p-0 mb-4 mb-lg-0 order-1 order-lg-2">
+                    <img loading="lazy" class="img-fluid"
+                      src=<?php echo get_template_directory_uri() . '/images/beneficios-mt/5.png' ?>
+                      alt="Gráfico">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="swiper-mobile">
+              <div class="container px-sm-5">
+                <div class="row d-flex align-items-center justify-content-center px-3 px-sm-5">
+                  <div class="col-12 col-lg-5 order-2 order-lg-1">
+                    <h3>Pressão arterial</h3>
+                    <div class="text-center d-block d-lg-none">
+                      <img loading="lazy" class="border-0"
+                        src=<?php echo get_template_directory_uri() . '/images/beneficios-mt/line.png' ?>
+                        alt="Linha">
+                    </div>
+                    <p>Uma meta-análise com 107 estudos, mostra que a prática da MT tem efeitos significativos na redução da pressão arterial sanguínea, em comparação com aplicação de outras técnicas não farmacológicas. 
+                    </p>
+                  </div>
+                  <div class="col-12 col-lg-7 d-flex align-items-center justify-content-center justify-content-lg-end p-0 mb-4 mb-lg-0 order-1 order-lg-2">
+                    <img loading="lazy" class="img-fluid"
+                      src=<?php echo get_template_directory_uri() . '/images/beneficios-mt/6.png' ?>
+                      alt="Gráfico">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="swiper-mobile">
+              <div class="container px-sm-5">
+                <div class="row d-flex align-items-center justify-content-center px-3 px-sm-5">
+                  <div class="col-12 col-lg-5 order-2 order-lg-1">
+                    <h3>Diabetes</h3>
+                    <div class="text-center d-block d-lg-none">
+                      <img loading="lazy" class="border-0"
+                        src=<?php echo get_template_directory_uri() . '/images/beneficios-mt/line.png' ?>
+                        alt="Linha">
+                    </div>
+                    <p>
+                      Uma  pesquisa realizada em colaboração com o National Institute of Health (NIH) e o governo norte-americano comprovou a eficácia da  MT na diminuição da resistência insulínica. 
+                    </p>
+                  </div>
+                  <div class="col-12 col-lg-7 d-flex align-items-center justify-content-center justify-content-lg-end p-0 mb-4 mb-lg-0 order-1 order-lg-2">
+                    <img loading="lazy" class="img-fluid"
+                      src=<?php echo get_template_directory_uri() . '/images/beneficios-mt/7.png' ?>
+                      alt="Gráfico">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="swiper-mobile">
+              <div class="container px-sm-5">
+                <div class="row d-flex align-items-center justify-content-center px-3 px-sm-5">
+                  <div class="col-12 col-lg-5 order-2 order-lg-1">
+                    <h3>Colesterol alto </h3>
+                    <div class="text-center d-block d-lg-none">
+                      <img loading="lazy" class="border-0"
+                        src=<?php echo get_template_directory_uri() . '/images/beneficios-mt/line.png' ?>
+                        alt="Linha">
+                    </div>
+                    <p>Um estudo da Kaiser Permanent Medical Center verificou que a taxa de colesterol total dos pacientes estudados diminuiu 30 mg/dl após 1 ano de prática da MT por 20 minutos, duas vezes ao dia.</p>
+                  </div>
+                  <div class="col-12 col-lg-7 d-flex align-items-center justify-content-center justify-content-lg-end p-0 mb-4 mb-lg-0 order-1 order-lg-2">
+                    <img loading="lazy" class="img-fluid"
+                      src=<?php echo get_template_directory_uri() . '/images/beneficios-mt/8.png' ?>
+                      alt="Gráfico">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="swiper-mobile">
+              <div class="container px-sm-5">
+                <div class="row d-flex align-items-center justify-content-center px-3 px-sm-5">
+                  <div class="col-12 col-lg-5 order-2 order-lg-1">
+                    <h3>Fortalecimento do sistema imunológico </h3>
+                    <div class="text-center d-block d-lg-none">
+                      <img loading="lazy" class="border-0"
+                        src=<?php echo get_template_directory_uri() . '/images/beneficios-mt/line.png' ?>
+                        alt="Linha">
+                    </div>
+                    <p>A MT também é eficaz para o fortalecimento do sistema imunológico, de acordo com a ciência. Uma pesquisa mostrou o aumento de glóbulos brancos em pacientes, o que ajuda a proteger o organismo de câncer e de infecções. 
+                    </p>
+                  </div>
+                  <div class="col-12 col-lg-7 d-flex align-items-center justify-content-center justify-content-lg-end p-0 mb-4 mb-lg-0 order-1 order-lg-2">
+                    <img loading="lazy" class="img-fluid"
+                      src=<?php echo get_template_directory_uri() . '/images/beneficios-mt/9.png' ?>
+                      alt="Gráfico">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="swiper-mobile">
+              <div class="container px-sm-5">
+                <div class="row d-flex align-items-center justify-content-center px-3 px-sm-5">
+                  <div class="col-12 col-lg-5 order-2 order-lg-1">
+                    <h3>Relacionamentos profissionais</h3>
+                    <div class="text-center d-block d-lg-none">
+                      <img loading="lazy" class="border-0"
+                        src=<?php echo get_template_directory_uri() . '/images/beneficios-mt/line.png' ?>
+                        alt="Linha">
+                    </div>
+                    <p>Um estudo realizado em uma indústria automobilística concluiu que, após três meses de prática regular da MT, funcionários apresentaram melhores relações no trabalho em comparação a outras pessoas da mesma empresa. 
+                    </p>
+                  </div>
+                  <div class="col-12 col-lg-7 d-flex align-items-center justify-content-center justify-content-lg-end p-0 mb-4 mb-lg-0 order-1 order-lg-2">
+                    <img loading="lazy" class="img-fluid"
+                      src=<?php echo get_template_directory_uri() . '/images/beneficios-mt/10.png' ?>
+                      alt="Gráfico">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="swiper-mobile">
+              <div class="container px-sm-5">
+                <div class="row d-flex align-items-center justify-content-center px-3 px-sm-5">
+                  <div class="col-12 col-lg-5 order-2 order-lg-1">
+                    <h3>Relacionamentos afetivos </h3>
+                    <div class="text-center d-block d-lg-none">
+                      <img loading="lazy" class="border-0"
+                        src=<?php echo get_template_directory_uri() . '/images/beneficios-mt/line.png' ?>
+                        alt="Linha">
+                    </div>
+                    <p>Estudantes universitários que praticaram o Programa de Meditação Transcendental® classificaram mais positivamente as pessoas importantes de suas vidas (pais, cônjuge) em comparação aos estudantes do grupo de controle.
+                    </p>
+                  </div>
+                  <div class="col-12 col-lg-7 d-flex align-items-center justify-content-center justify-content-lg-end p-0 mb-4 mb-lg-0 order-1 order-lg-2">
+                    <img loading="lazy" class="img-fluid"
+                      src=<?php echo get_template_directory_uri() . '/images/beneficios-mt/11.png' ?>
+                      alt="Gráfico">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="swiper-mobile">
+              <div class="container px-sm-5">
+                <div class="row d-flex align-items-center justify-content-center px-3 px-sm-5">
+                  <div class="col-12 col-lg-5 order-2 order-lg-1">
+                    <h3>Autorrealização</h3>
+                    <div class="text-center d-block d-lg-none">
+                      <img loading="lazy" class="border-0"
+                        src=<?php echo get_template_directory_uri() . '/images/beneficios-mt/line.png' ?>
+                        alt="Linha">
+                    </div>
+                    <p>Uma metanálise de todos os estudos científicos já realizados sobre os efeitos de diferentes técnicas de meditação na realização pessoal mostrou que a MT apresenta eficácia de 3 a 4 vezes maior que outras técnicas de meditação e de relaxamento. 
+                    </p>
+                  </div>
+                  <div class="col-12 col-lg-7 d-flex align-items-center justify-content-center justify-content-lg-end p-0 mb-4 mb-lg-0 order-1 order-lg-2">
+                    <img loading="lazy" class="img-fluid"
+                      src=<?php echo get_template_directory_uri() . '/images/beneficios-mt/12.png' ?>
+                      alt="Gráfico">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="swiper-mobile">
+              <div class="container px-sm-5">
+                <div class="row d-flex align-items-center justify-content-center px-3 px-sm-5">
+                  <div class="col-12 col-lg-5 order-2 order-lg-1">
+                    <h3>Criatividade e outras habilidades </h3>
+                    <div class="text-center d-block d-lg-none">
+                      <img loading="lazy" class="border-0"
+                        src=<?php echo get_template_directory_uri() . '/images/beneficios-mt/line.png' ?>
+                        alt="Linha">
+                    </div>
+                    <p>
+                      Depois de um período de 6 a 12 meses de prática regular de MT, um grupo de estudantes de Taiwan apresentou melhora significativa em cinco escalas diferentes de capacidade intelectual, incluindo uma escala de “toda a criatividade cerebral”. 
+                    </p>
+                  </div>
+                  <div class="col-12 col-lg-7 d-flex align-items-center justify-content-center justify-content-lg-end p-0 mb-4 mb-lg-0 order-1 order-lg-2">
+                    <img loading="lazy" class="img-fluid"
+                      src=<?php echo get_template_directory_uri() . '/images/beneficios-mt/13.png' ?>
+                      alt="Gráfico">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="swiper-mobile">
+              <div class="container px-sm-5">
+                <div class="row d-flex align-items-center justify-content-center px-3 px-sm-5">
+                  <div class="col-12 col-lg-5 order-2 order-lg-1">
+                    <h3>Desenvolvimento da inteligência </h3>
+                    <div class="text-center d-block d-lg-none">
+                      <img loading="lazy" class="border-0"
+                        src=<?php echo get_template_directory_uri() . '/images/beneficios-mt/line.png' ?>
+                        alt="Linha">
+                    </div>
+                    <p>
+                      Um estudo feito com estudantes universitários praticantes de MT mostrou  que eles obtiveram um aumento significativo do QI, enquanto o grupo de controle não apresentou avanços. 
+                    </p>
+                    <p>
+                      A pesquisa comprovou que adultos com mais de 18 anos podem elevar a inteligência com a ajuda da técnica, ao contrário do que se acreditava anteriormente. 
+                    </p>
+                  </div>
+                  <div class="col-12 col-lg-7 d-flex align-items-center justify-content-center justify-content-lg-end p-0 mb-4 mb-lg-0 order-1 order-lg-2">
+                    <img loading="lazy" class="img-fluid"
+                      src=<?php echo get_template_directory_uri() . '/images/beneficios-mt/14.png' ?>
+                      alt="Gráfico">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+      </div>
+    </div>
+  </section>
---
+++ template-parts/pages/beneficios-2/footer.php
@@ -0,0 +1,10 @@
+<section class="footer">
+    <div class="container">
+      <h2><span>Dê agora</span> o primeiro passo para aprender a técnica e experimentar bem-estar pleno.</h2>
+      <div class="text-center">
+        <a href="<?php echo home_url( '/agendamento/' ); ?>" class="btn" role="button">
+            Agende palestra online gratuita.
+        </a>
+      </div>
+    </div>
+  </section>
