7f3d4fb6246902d3cfec55d8c4d82b978b76ca8d
Bruna Rafaela <brunarafaelav@outlook.com>
2022-11-22 07:20:39 -0300

    update page beneficios 2 header

--- template-parts/headers/header-beneficios-2.php
+++ template-parts/headers/header-beneficios-2.php
@@ -1,9 +1,6 @@
 <header class="header header--beneficios-2">
-<<<<<<< Updated upstream
-  <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
-=======
   <div class="carousel slide">
->>>>>>> Stashed changes
+  <div class="carousel slide">
     <div class="carousel-inner">
       <div class="carousel-item active text-end">
         <div class="carousel-caption d-flex align-items-center justify-content-center">
@@ -14,13 +11,8 @@
             </h5>
           </div>
         </div>
-<<<<<<< Updated upstream
         <img loading="lazy" class="d-none d-lg-inline-block img-fluid" src="<?php echo get_template_directory_uri() . '/images/header/banner-beneficios-2.png' ?>" alt="Benefícios">
         <img loading="lazy" class="d-inline-block d-lg-none img-fluid" src="<?php echo get_template_directory_uri() . '/images/header/banner-beneficios-2-mobile.png' ?>" alt="Benefícios">
-=======
-        <img loading="lazy" class="d-none d-lg-block img-fluid" src="<?php echo get_template_directory_uri() . '/images/header/banner-beneficios-2.png' ?>" alt="Benefícios">
-        <img loading="lazy" class="d-block d-lg-none img-fluid" src="<?php echo get_template_directory_uri() . '/images/header/banner-beneficios-2-mobile.png' ?>" alt="Benefícios">
->>>>>>> Stashed changes
       </div>
     </div>
   </div>
