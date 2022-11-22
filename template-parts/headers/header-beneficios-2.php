84f69d1eaac26fccf33fd55137216619de5a7e5c
Bruna Rafaela <brunarafaelav@outlook.com>
2022-11-21 22:07:15 -0300

    update header page beneficios 2

--- template-parts/headers/header-beneficios-2.php
+++ template-parts/headers/header-beneficios-2.php
@@ -1,10 +1,5 @@
 <header class="header header--beneficios-2">
   <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
-    <!-- <div class="carousel-indicators">
-      <button name="btn-1" type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="" aria-label="Slide 1"></button>
-      <button name="btn-2" type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" class="active" aria-current="true"></button>
-      <button name="btn-3" type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    </div> -->
     <div class="carousel-inner">
       <div class="carousel-item active text-end">
         <div class="carousel-caption d-flex align-items-center justify-content-center">
@@ -15,16 +10,9 @@
             </h5>
           </div>
         </div>
-        <img loading="lazy" class="img-fluid" src="<?php echo get_template_directory_uri() . '/images/header/banner-beneficios-2.png' ?>" alt="Benefícios">
+        <img loading="lazy" class="d-none d-lg-inline-block img-fluid" src="<?php echo get_template_directory_uri() . '/images/header/banner-beneficios-2.png' ?>" alt="Benefícios">
+        <img loading="lazy" class="d-inline-block d-lg-none img-fluid" src="<?php echo get_template_directory_uri() . '/images/header/banner-beneficios-2-mobile.png' ?>" alt="Benefícios">
       </div>
     </div>
-    <!-- <button name="btn-previous" class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button name="btn-next" class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button> -->
   </div>
 </header>
