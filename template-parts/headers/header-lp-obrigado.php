37d054fe1396803f01b458ba7250d503d20e2d3e
Becky Marques <becky.marques@colmeiaperformance.com.br>
2022-12-21 13:04:40 -0300

    [Fix] header logo img

--- template-parts/headers/header-lp-obrigado.php
+++ template-parts/headers/header-lp-obrigado.php
@@ -48,7 +48,7 @@
     <div class="row">
       <div class="col-12 col-lg d-flex flex-column justify-content-around text-white">
         <a class="navbar-brand" href="<?php echo home_url(); ?>">
-          <img class="d-none d-sm-block " loading="lazy"
+          <img class="img-fluid w-50 d-none d-sm-block " loading="lazy"
             src=<?php echo get_template_directory_uri() . '/images/header/logo-topo.png' ?>
             alt="Logotipo Meditação Transcedental">
           <img class="d-block d-sm-none " loading="lazy"
