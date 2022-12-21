e0d8688378c2ca558b705efecc3f95c4b27ec205
Bruna Rafaela <brunarafaelav@outlook.com>
2022-12-21 09:20:37 -0300

    Update: add visite-blog section loop

--- page-lp-obrigado.php
+++ page-lp-obrigado.php
@@ -2,12 +2,7 @@
 <?php get_template_part('template-parts/headers/header-lp-obrigado'); ?>
 <main class="page py-5">
   <div class="container">
-
     <?php get_template_part('template-parts/pages/home/visite-blog'); ?>
-
   </div>
-
-
 </main>
-
 <?php get_footer() ?>
--- template-parts/pages/home/visite-blog.php
+++ template-parts/pages/home/visite-blog.php
@@ -3,52 +3,40 @@
       <h2>Visite o nosso <strong>blog</strong></h2>
       <div id="conhecaBlog" class="swiper swiperConhecaBlog" data-bs-ride="carousel">
         <div class="swiper-wrapper">
-
+        <?php $oquedizem_query = new WP_Query( 'posts_per_page=4' );
+             while ( $oquedizem_query->have_posts() ) : $oquedizem_query->the_post();
+              $do_not_duplicate = $post->ID; ?>
           <div class="swiper-slide col-12 col-lg-6">
             <div class="p-3 col d-flex h-100">
               <div class="post p-3 d-lg-flex">
                 <div class="post-img col mb-2 mb-lg-0 col-lg-5">
-                  <img loading="lazy" class="img-fluid" src="https://picsum.photos/800/500?random=1" alt="">
+                <?php 
+                  if ( has_post_thumbnail() ) { 
+                      echo the_post_thumbnail();
+                  }
+                  else { 
+                  echo get_template_directory_uri() . '/images/blog-media.jpg';
+                  } ?>
                 </div>
                 <div class="col post-item d-flex flex-column text-center text-lg-start">
                   <div class="item-content">
                     <p>
-                      <strong>Quem foi Maharishi Mahesh Yogi, o fundador da Meditação Transcendental®</strong></p>
+                      <strong><?php the_title(); ?></strong></p>
                     <p>
-                      Conheça o cientista Dr. Tony Nader, chefe da Organização Internacional de Meditação Transcendental®
+                      <?php echo wp_trim_words(get_the_excerpt(), 15); ?>
                     </p>
                   </div>
                   <div class="item-btn d-flex align-items-end justify-content-center justify-content-lg-end">
-                    <a class="btn visite-blog-btn text-uppercase" href="#" role="button">Leia +</a>
+                    <a class="btn visite-blog-btn text-uppercase" href="<?php the_permalink(); ?>" role="button">Leia +</a>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-
-          <div class="swiper-slide col-12 col-lg-6">
-            <div class="p-3 col d-flex h-100">
-              <div class="post p-3 d-lg-flex">
-                <div class="post-img col mb-2 mb-lg-0 col-lg-5">
-                  <img loading="lazy" class="img-fluid" src="https://picsum.photos/800/500?random=1" alt="">
-                </div>
-                <div class="col post-item d-flex flex-column text-center text-lg-start">
-                  <div class="item-content">
-                    <p>
-                      <strong>Quem foi Maharishi Mahesh Yogi, o fundador da Meditação Transcendental®</strong></p>
-                    <p>
-                    Conheça o cientista Dr. Tony Nader, chefe da Organização Internacional de Meditação Transcendental®Conheça o cientista Dr. Tony Nader, chefe da Organização Internacional de Meditação Transcendental®Conheça o cientista Dr. Tony Nader, chefe da Organização Internacional de Meditação Transcendental®
-                    </p>
-                  </div>
-                  <div class="item-btn d-flex align-items-end justify-content-center justify-content-lg-end">
-                    <a class="btn visite-blog-btn text-uppercase" href="#" role="button">Leia +</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          
+          <?php endwhile; 
+            wp_reset_postdata();
+          ?>
+      
         </div>
         <div class="swiper-button-next"></div>
         <div class="swiper-button-prev"></div>
