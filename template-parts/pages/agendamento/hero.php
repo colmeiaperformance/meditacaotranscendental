5486a839f905879b78aa63e1597bbef6e78e98ea
Bruna Rafaela <brunarafaelav@outlook.com>
2023-01-19 04:36:26 -0300

    Update page-agendamento-2

--- template-parts/pages/agendamento/hero.php
+++ template-parts/pages/agendamento/hero.php
@@ -1,7 +1,7 @@
 <section class="page-agendamento--hero">
     <div class="container">
-        <h3><span>Inscreva-se na aula introdutória online e gratuita</span> para conhecer a <span>Meditação Transcendental</span></h3>
-        <h3>Selecione abaixo seu <strong>Estado</strong> e <strong>Cidade</strong> e veja as datas de sua preferência</h3>
+        <h4><span>Inscreva-se na aula introdutória online e gratuita</span> para conhecer a <span>Meditação Transcendental</span></h4>
+        <h4>Selecione abaixo seu <strong>Estado</strong> e <strong>Cidade</strong> e veja as datas de sua preferência</h4>
     </div>
 </div>
 </section>
