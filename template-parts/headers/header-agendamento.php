a1f5b80202462d9cec1f981e12ad6b9b68dacb13
Becky Marques <becky.marques@colmeiaperformance.com.br>
2023-01-20 10:17:43 -0300

    [Update] Improvements responsive lp agendamento

--- template-parts/headers/header-agendamento.php
+++ template-parts/headers/header-agendamento.php
@@ -1,7 +1,7 @@
 <header class="header header--agendamento">
   <div class="">
     <div class="row">
-      <div class="col-6 text">
+      <div class="col-lg-6 text">
         <h1>
           Comece agora a sua jornada rumo ao equilíbrio e autoconhecimento
         </h1>
@@ -9,9 +9,9 @@
           A Meditação Transcendental® (MT) é uma técnica simples, fácil e natural, que remove o estresse, promove saúde, paz interior, bem-estar e equilíbrio.
         </h5>
       </div>
-      <div class="col-6 image">
+      <div class="col-lg-6 image">
         <img loading="lazy" class="d-none d-lg-inline-block img-fluid" src="<?php echo get_template_directory_uri() . '/images/header/banner-agendamento.png' ?>" alt="Agendamento">
-        <img loading="lazy" class="d-inline-block d-lg-none img-fluid" src="<?php echo get_template_directory_uri() . '/images/header/banner-agendamento.png' ?>" alt="Agendamento">
+        <img loading="lazy" class="d-inline-block d-lg-none img-fluid" src="<?php echo get_template_directory_uri() . '/images/header/banner-agendamento-mobile.png' ?>" alt="Agendamento">
       <div>               
     <div>
   </div>
