6842eddbb1f018710364d1f04e430584b2140e84
Becky Marques <becky.marques@colmeiaperformance.com.br>
2023-01-24 16:37:02 -0300

    [Update] Add popper

--- functions.php
+++ functions.php
@@ -12,6 +12,7 @@ function loading_styles(){
 
 //Scripts
 function loading_scripts(){
+    wp_register_script( 'popper-js', 'https://unpkg.com/@popperjs/core@2', array( 'jquery-core' ), wp_get_theme()->get( 'Version' ), true  );
     wp_register_script( 'bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js', array( 'jquery-core' ), wp_get_theme()->get( 'Version' ), true  );
     wp_register_script( 'swiper-js', get_template_directory_uri() . '/vendor/swiper-js/swiper-bundle.min.js', array( 'jquery-core' ), wp_get_theme()->get( 'Version' ), true  );
     wp_register_script( 'typed-js', get_template_directory_uri() . '/vendor/typed-js/typed.min.js', array( 'jquery-core' ), wp_get_theme()->get( 'Version' ), true  );
@@ -21,6 +22,7 @@ function loading_scripts(){
     wp_register_script( 'jquery-counterUp-js', get_template_directory_uri() . '/js/jquery.counterup2.min.js', array( 'jquery-core' ), wp_get_theme()->get( 'Version' ), true  );
     wp_register_script( 'main-js', get_template_directory_uri() . '/js/main.js', array( 'jquery-core' ), wp_get_theme()->get( 'Version' ), true  );
     
+    wp_enqueue_script( 'popper-js');
     wp_enqueue_script( 'bootstrap-js');
     wp_enqueue_script( 'swiper-js');
     wp_enqueue_script( 'typed-js');
