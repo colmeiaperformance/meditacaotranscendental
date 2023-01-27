0cf93daee2686fd55cac9d884a1b15ca6b9c5d78
Becky Marques <becky.marques@colmeiaperformance.com.br>
2023-01-27 03:41:18 -0300

    [Update] Adjustments responsive lp agendamento

--- template-parts/pages/agendamento/hero.php
+++ template-parts/pages/agendamento/hero.php
@@ -1,16 +1,16 @@
 <section class="page-agendamento--hero pb-0 pb-lg-4">
   <div class="container">
     <h4>
-        <strong>
-            <span>
-                Inscreva-se na aula introdutória online e gratuita
-            </span>
-        </strong>
+      <strong>
+        <span>
+          Inscreva-se na aula introdutória online e gratuita
+        </span>
+      </strong>
     </h4>
-    <h4 class="py-0">
+    <p class="lead py-0">
       Selecione abaixo seu <strong>Estado</strong> e <strong>Cidade</strong> e veja as datas de sua
       preferência
-    </h4>
+    </p>
   </div>
   </div>
 </section>
